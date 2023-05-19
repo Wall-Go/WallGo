213983aad0fb9f55ecb878ae3ea327990ae5c238
Hirvonen <joonashi@ad.helsinki.fi>
2023-05-19 16:33:06 +0300

    Initializing the code for wall EoM and hydro

---
+++ src/WallSpeed/eomHydro.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+def findWallVelocityLoop(TNucl, wallVelocityLTE, hMass, sMass, errTol):
+
+    # Initial conditions
+
+    higgsWidth = 1/hMass
+    singletWidth = 1/sMass
+    wallOffSet = 0
+
+    if wallVelocityLTE is not None:
+        wallVelocity = 0.9 * wallVelocityLTE
+    else:
+        wallVelocity = np.sqrt(1/3)
+
+    offEquilDeltas = 0
+
+    c1, c2 = findHydroBoundaries(TNucl, wallVelocity)
+    Tprofile = findTemperatureProfile(c1, c2, higgsWidth, singletWidth, wallOffSet, offEquilDeltas)
+
+    error = errTol + 1
+    while error > errTol:
+
+        oldHiggsWidth = higgsWidth
+        oldSingletWidth = singletWidth
+        oldWallOffSet = wallOffSet
+        oldWallVelocity = wallVelocity
+
+        offEquilDeltas = solveBoltzmannEquation(Tprofile, higgsWidth, singletWidth, wallOffSet)
+
+        c1, c2 = findHydroBoundaries(TNucl, wallVelocity)
+        Tprofile = findTemperatureProfile(c1, c2, higgsWidth, singletWidth, wallOffSet, offEquilDeltas)
+
+        wallVelocity, higgsWidth, singletWidth, wallOffSet = solveWallEoM()
+
+        error = np.sqrt(((wallVelocity-oldWallVelocity)/wallVelocity)**2 + ((higgsWidth-oldHiggsWidth)/higgsWidth)**2 + ((singletWidth-oldSingletWidth)/singletWidth)**2 + (wallOffSet-oldWallOffSet)**2)
+
+    return wallVelocity, higgsWidth, singletWidth, wallOffSet
