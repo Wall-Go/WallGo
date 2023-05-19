ccc0b57322bd5edea8efda64266cd4c56cd23e40
Philipp Schicho <schicho@icloud.com>
2023-05-19 15:42:35 +0200

    init Veff module

---
+++ src/WallSpeed/veff.py
@@ -0,0 +1,21 @@
+import numpy as np
+import math
+import warnings
+#import model
+import FiniteT
+
+
+class Veff:
+    '''
+    Class that generates the effective potential given a pre-defined model
+    '''
+
+    def Veff4d(self):
+        '''
+        Thermal effective potential at 1 loop in 4d
+        '''
+
+    def Veff3d(self):
+        '''
+        Thermal effective potential at 1 loop at the supersoft scale
+        '''
