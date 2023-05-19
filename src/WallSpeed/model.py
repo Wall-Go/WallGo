eea89a4982fea1a0387e123d737bee95859b0ec2
Philipp Schicho <schicho@icloud.com>
2023-05-19 18:00:14 +0200

    rough outline of functions

---
+++ src/WallSpeed/model.py
@@ -0,0 +1,8 @@
+import numpy as np
+import math
+import warnings
+
+class Model:
+    '''
+    Class that generates the model given external model file
+    '''
--- src/WallSpeed/veff.py
+++ src/WallSpeed/veff.py
@@ -1,7 +1,7 @@
 import numpy as np
 import math
 import warnings
-#import model
+import model
 import FiniteT
 
 
@@ -9,6 +9,37 @@ class Veff:
     '''
     Class that generates the effective potential given a pre-defined model
     '''
+
+    def __init__(self):
+        '''
+        Initialise class
+        '''
+
+    def J3(self,m):
+        '''
+        Log integral for 1loop 3d Veff
+        '''
+        return -m**3/(12*np.pi)
+
+
+
+    def Vtree(self,X,show_V=False):
+        X = np.asanyarray(X)
+        h,s = X[...,0],X[...,1]
+        vtree = (
+            +self.muh2*h**2/2
+            +self.mus2*s**2/2
+            +self.lambdaH*h**4/4
+            +self.lambdaS*s**4/4
+            +self.lambdaHS*(h*s)**2/4
+            +self.CTs[0]
+            +self.CTs[1]*h**2
+            +self.CTs[2]*s**2
+            +self.CTs[3]*h**4)
+        if show_V:
+            print(vtree)
+        return vtree
+
 
     def Veff4d(self):
         '''
@@ -18,4 +49,19 @@ class Veff:
     def Veff3d(self):
         '''
         Thermal effective potential at 1 loop at the supersoft scale
+        '''
+
+    def dVdT(self,X,T,include_radiation=True):
+        '''
+        1st T-derivative of the effective potential
+        '''
+
+    def d2VdT2(self,X,T,include_radiation=True):
+        '''
+        2nd T-derivative of the effective potential
+        '''
+
+    def d3VdT3(self,X,T,include_radiation=True):
+        '''
+        3rd T-derivative of the effective potential
         '''
