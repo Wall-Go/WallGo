39fb46b3eafb66944ddcaadbda888ced7c35aa6a
Jorinde van de Vis <jorindevandevis@outlook.com>
2023-05-21 18:07:21 +0200

    Added function that computes the Jouguet velocity

--- src/WallSpeed/eomHydro.py
+++ src/WallSpeed/eomHydro.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.optimize import fsolve
 
 def findWallVelocityLoop(Model,TNucl, wallVelocityLTE, hMass, sMass, errTol):
 
@@ -36,3 +37,27 @@ def findWallVelocityLoop(Model,TNucl, wallVelocityLTE, hMass, sMass, errTol):
         error = np.sqrt(((wallVelocity-oldWallVelocity)/wallVelocity)**2 + ((higgsWidth-oldHiggsWidth)/higgsWidth)**2 + ((singletWidth-oldSingletWidth)/singletWidth)**2 + (wallOffSet-oldWallOffSet)**2)
 
     return wallVelocity, higgsWidth, singletWidth, wallOffSet
+
+
+def findJouguetVelocity(Model,Tnucl):
+        """
+        Finds the Jouguet velocity for a thermal effective potential, defined by Model,
+        using that the derivative of v+ wrt T- is zero at the Jouguet velocity
+        """
+        def vpDerivNum(tm): #the numerator of the derivative of v+^2
+            num1 = Model.pSym(Tnucl) - Model.pBrok(tm) #first factor in the numerator of v+^2
+            num2 = Model.pSym(Tnucl) + Model.eBrok(tm) 
+            den1 = Model.eSym(Tnucl) - Model.eBrok(tm) #first factor in the denominator of v+^2
+            den2 = Model.eSym(Tnucl) + Model.pBrok(tm) 
+            dnum1 = - Model.dpBrok(tm) #T-derivative of first factor wrt tm
+            dnum2 = Model.deBrok(tm)
+            dden1 = - Model.deBrok(tm) #T-derivative of second factor wrt tm
+            dden2 = Model.dpBrok(tm)
+            return(dnum1*num2*den1*den2 + num1*dnum2*den1*den2 - num1*num2*dden1*den2 - num1*num2*den1*dden2)
+
+        tmSol = fsolve(vpDerivNum,Tnucl*1.1)[0]
+        vp = np.sqrt((Model.pSym(Tnucl) - Model.pBrok(tmSol))*(Model.pSym(Tnucl) + Model.eBrok(tmSol))/(Model.eSym(Tnucl) - Model.eBrok(tmSol))/(Model.eSym(Tnucl) + Model.pBrok(tmSol)))
+        return(vp)
+
+        
+        
