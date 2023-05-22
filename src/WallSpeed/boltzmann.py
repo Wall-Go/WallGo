053a0c439ab5dbad20316db92fc16ad71d0d9122
og113 <oligould@zoho.com>
2023-05-22 14:18:21 +0200

    Added possible initialisation of BoltzmannSolver

--- src/WallSpeed/boltzmann.py
+++ src/WallSpeed/boltzmann.py
@@ -1,28 +1,42 @@
 import numpy as np
 import h5py # read/write hdf5 structured binary data file format
-from .coordinates import ...
+from .Grid import Grid
 
 class BoltzmannSolver:
     """
     Class for solving Boltzmann equations for small deviations from equilibrium.
-
     """
 
-    def __init__():
+    def __init__(self, grid, background, mode, collisionFile):
         """
         Initialsation of BoltzmannSolver
 
         Parameters
         ----------
+        grid : Grid
+            An object of the Grid class.
+        background : Background
+            An object of the Background class.
+        mode : Mode
+            An object of the Mode class.
+        collisionFile : string
+            File name where collision integrals are stored.
 
         Returns
         -------
         cls : BoltzmannSolver
             An object of the BoltzmannSolver class.
         """
-        cls = BoltzmannSolver()
+        self.grid = grid
+        self.background = background
+        self.mode = mode
+        try:
+            with h5py.File(collisionFile, "r") as file:
+                self.collision = np.array(file["random"])
+        except FileNotFoundError:
+            print("BoltzmannSolver error: %s not found" % collisionFile)
+            raise
 
-        return cls
 
     def solveBoltzmannEquations():
         """
@@ -36,11 +50,17 @@ class BoltzmannSolver:
         delta_f : array_like
             The deviation from equilibrium, a rank 6 array, with shape
             :py:data:`(len(z), len(pz), len(pp), len(z), len(pz), len(pp))`.
+
+        References
+        ----------
+        .. [LC22] B. Laurent and J. M. Cline, First principles determination
+            of bubble wall velocity, Phys. Rev. D 106 (2022) no.2, 023501
+            doi:10.1103/PhysRevD.106.023501
         """
         # contructing the various terms in the Boltzmann equation
-        source = self.__source(z, pz, pp)
-        liouville = self.__liouville(z, pz, pp)
-        collision = self.__collision(z, pz, pp)
+        source = self.source(z, pz, pp)
+        liouville = self.liouville(z, pz, pp)
+        collision = self.collision(z, pz, pp)
 
         # constructing the full rank 6 tensor operator
         operator = liouville + collision[np.newaxis, :, :, np.newaxis, :, :]
@@ -70,36 +90,30 @@ class BoltzmannSolver:
         Deltas : array_like
             Defined in equation (20) of [LC22]_. A list of 4 arrays, each of
             which is of size :py:data:`len(z)`.
-
-        References
-        ----------
-        .. [LC22] B. Laurent and J. M. Cline, First principles determination
-            of bubble wall velocity, Phys. Rev. D 106 (2022) no.2, 023501
-            doi:10.1103/PhysRevD.106.023501
         """
         pass
 
-    def __collocationGrid(Nz, Npz, Npp):
-        """
-        Constructs a collocation grid of Gauss-Lobatto points.
-
-        See equation (34) of [LC22].
-        """
-        z_compact = np.cos(np.pi * np.arange(1, Nz) / Nz)
-        pz_compact = np.cos(np.pi * np.arange(1, Npz) / Npz)
-        pp_compact = np.cos(np.pi * np.arange(1, Npp) / (Npp - 1))
-        return z_compact, pz_compact, pp_compact
-
-    def __source(z, pz, pp, vw, msq, T, statistics=-1):
+    def source(vw):
         """
         Local equilibrium source term for non-equilibrium deviations, a
         rank 3 array, with shape :py:data:`(len(z), len(pz), len(pp))`.
         """
+        # coordinates
+        chi, rz, rp = self.grid.getCompactCoordinates()
+
+        # background profiles
+        T = self.background.temperatureProfile
+        field = self.background.fieldProfile
+
+        # fluctuation mode
+        statistics = self.mode.statistics
+        msq = self.mode.msq(field)
+
         # evaluating dot products with the plasma 4-velocity
         gamma = 1 / np.sqrt(1 - vw**2)
-        E_wall = msq + pz**2 + pp**2
-        E_plasma = gamma * (E_wall - vw * pz)
-        P_plasma = gamma * (- vw * E_wall + pz)
+        E_wall = np.sqrt(msq + rz**2 + rp**2)
+        E_plasma = gamma * (E_wall - vw * rz)
+        P_plasma = gamma * (- vw * E_wall + rz)
 
         # equilibrium distribution, and its derivative
         f_eq = 1 / (np.exp(E_plasma / T) - statistics * 1)
@@ -112,7 +126,7 @@ class BoltzmannSolver:
         # putting it together
         pass
 
-    def __liouville(z, pz, pp):
+    def liouville(z, pz, pp):
         """
         Lioville operator, a rank 6 array, with shape
         :py:data:`(len(z), len(pz), len(pp), len(z), len(pz), len(pp))`.
@@ -120,7 +134,7 @@ class BoltzmannSolver:
         pass
 
 
-    def __collision(z, pz, pp):
+    def collision(z, pz, pp):
         """
         Collision integrals, a rank 4 array, with shape
         :py:data:`(len(pz), len(pp), len(pz), len(pp))`.
