8ef589bfc601db551c46747765dd4918cd223e1c
Jorinde van de Vis <jorindevandevis@outlook.com>
2023-05-24 13:36:58 +0200

    Moved test to test folder and added TestModel class

---
+++ tests/test_Hydro.py
@@ -0,0 +1,14 @@
+import pytest
+import TestModel
+from WallSpeed.Hydro import *
+
+#model1 = TestModel.TestModel(0.2,0.1,0.4)
+
+#print(model1.eBrok(0.3))
+
+#print(findJouguetVelocity(model1,0.7))
+
+def test_JouguetVelocity():
+    model1 = TestModel.TestModel(0.2,0.1,0.4)
+    res = findJouguetVelocity(model1,0.7)
+    assert res == pytest.approx(0.7240,rel=0.01)
