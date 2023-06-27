75cdc5cc26d24cf212ea58986b92ae5d309ae81c
Hirvonen <76170033+joonashir@users.noreply.github.com>
2023-06-27 10:01:00 +0300

    Copied initial wall parameters to the test file

--- tests/test_eomMockPotential.py
+++ tests/test_eomMockPotential.py
@@ -1,7 +1,53 @@
-#from WallSpeed.eomHydro import initialWallParameters
-from eomHydro import initialWallParameters, wallProfile
+from scipy.integrate import quad
+from scipy.optimize import minimize, brentq, root
 import numpy as np
 import matplotlib.pyplot as plt
+
+def initialWallParameters(
+    higgsWidthGuess, singletWidthGuess, wallOffSetGuess, TGuess, Veff
+):
+    higgsVEV = Veff.higgsVEV(TGuess)
+    singletVEV = Veff.singletVEV(TGuess)
+
+    initRes = minimize(
+        lambda wallParams: oneDimAction(higgsVEV, singletVEV, wallParams, TGuess, Veff),
+        x0=[higgsWidthGuess, singletWidthGuess, wallOffSetGuess],
+        bounds=[(0, None), (0, None), (-10, 10)],
+    )
+
+    return initRes.x[0], initRes.x[1], initRes.x[2]
+
+def oneDimAction(higgsVEV, singletVEV, wallParams, T, Veff):
+    [higgsWidth, singletWidth, wallOffSet] = wallParams
+
+    kinetic = (1 / higgsWidth + 1 / singletWidth) * 3 / 2
+
+    integrationLength = (20 + np.abs(wallOffSet)) * max(higgsWidth, singletWidth)
+
+    integral = quad(
+        lambda z: Veff.V(
+            wallProfile(higgsVEV, singletVEV, higgsWidth, singletWidth, wallOffSet, z),
+            T,
+        ),
+        -integrationLength,
+        integrationLength,
+    )
+
+    potential = integral[0] - integrationLength * (
+        Veff.V([higgsVEV, 0], T) + Veff.V([0, singletVEV], T)
+    )
+
+    print(higgsWidth, singletWidth, wallOffSet)
+
+    print(kinetic + potential)
+
+    return kinetic + potential
+
+def wallProfile(higgsVEV, singletVEV, higgsWidth, singletWidth, wallOffSet, z):
+    h = 0.5 * higgsVEV * (1 - np.tanh(z / higgsWidth))
+    s = 0.5 * singletVEV * (1 + np.tanh(z / singletWidth + wallOffSet))
+
+    return [h, s]
 
 
 class MockPotential:
