9668b0344e8218364a889d3ba53bdce22cff009a
Benoit <benoit.laurent96@gmail.com>
2023-07-06 16:12:01 -0400

    Made sure all the tests pass.

--- tests/test_HydroTemplateModel.py
+++ tests/test_HydroTemplateModel.py
@@ -8,14 +8,14 @@ from WallSpeed.HydroTemplateModel import *
 
 #These tests are all based on a comparison between the classes HydroTemplateModel and Hydro used with TestTemplateModel
 N = 100
-rng = np.random.default_rng(10)
+rng = np.random.default_rng(6)
 
 def test_JouguetVelocity():
     res1,res2 = np.zeros(N),np.zeros(N)
     psiN = 1-0.5*rng.random(N)
     alN = (1-psiN)/3+rng.random(N)
-    cb2 = 1/4+(1/3-1/4)*rng.random(N)
     cs2 = 1/4+(1/3-1/4)*rng.random(N)
+    cb2 = cs2-(1/3-1/4)*rng.random(N)
     for i in range(N):
         model = TestModel.TestModelTemplate(alN[i],psiN[i],cb2[i],cs2[i])
         hydro = Hydro(model,1)
@@ -28,8 +28,8 @@ def test_findMatching():
     res1,res2 = np.zeros((N,4)),np.zeros((N,4))
     psiN = 1-0.5*rng.random(N)
     alN = (1-psiN)/3+rng.random(N)
-    cb2 = 1/4+(1/3-1/4)*rng.random(N)
     cs2 = 1/4+(1/3-1/4)*rng.random(N)
+    cb2 = cs2-(1/3-1/4)*rng.random(N)
     vw = rng.random(N)
     for i in range(N):
         model = TestModel.TestModelTemplate(alN[i],psiN[i],cb2[i],cs2[i])
@@ -47,9 +47,10 @@ def test_findvwLTE():
     res1,res2 = np.zeros(N),np.zeros(N)
     psiN = 1-0.5*rng.random(N)
     alN = (1-psiN)/3+rng.random(N)
-    cb2 = 1/4+(1/3-1/4)*rng.random(N)
     cs2 = 1/4+(1/3-1/4)*rng.random(N)
+    cb2 = cs2-(1/3-1/4)*rng.random(N)
     for i in range(N):
+        print(alN[i],psiN[i],cb2[i],cs2[i])
         model = TestModel.TestModelTemplate(alN[i],psiN[i],cb2[i],cs2[i])
         hydro = Hydro(model,1)
         hydroTemplate = HydroTemplateModel(model,1)
@@ -61,8 +62,8 @@ def test_findHydroBoundaries():
     res1,res2 = np.zeros((N,4)),np.zeros((N,4))
     psiN = 1-0.5*rng.random(N)
     alN = (1-psiN)/3+rng.random(N)
-    cb2 = 1/4+(1/3-1/4)*rng.random(N)
     cs2 = 1/4+(1/3-1/4)*rng.random(N)
+    cb2 = cs2-(1/3-1/4)*rng.random(N)
     vw = rng.random(N)
     for i in range(N):
         model = TestModel.TestModelTemplate(alN[i],psiN[i],cb2[i],cs2[i])
