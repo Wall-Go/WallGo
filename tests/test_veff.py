bb61bad56e7b9cf25c738c25070133542643ee5f
Philipp Schicho <schicho@icloud.com>
2023-08-22 10:00:16 +0200

    include another BM

--- tests/test_veff.py
+++ tests/test_veff.py
@@ -13,8 +13,8 @@ def test_BM1():
     assert mod.Vtot([[110,130]],100) == pytest.approx([-1.15450678e+09],rel=0.01)
     # assert res == pytest.approx([196.734, -0.2536],rel=0.01)
 
-# def test_BM2():
-#     pot = Model(1,125,160,1.0,1.6)
-#     res = pot.findMinimum(None,100)
-#     assert res == pytest.approx([4.4e-9, -146.758],rel=0.01)
-
+def test_BM2():
+    mod = Model(125,160,1.0,1.6)
+    # res = pot.findMinimum(None,100)
+    assert mod.Vtot([[110,130]],100) == pytest.approx([-1.23684861e+09],rel=0.01)
+    # assert res == pytest.approx([4.4e-9, -146.758],rel=0.01)
