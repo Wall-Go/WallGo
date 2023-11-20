loop over massSquared and dofs in V1T to fix broadcasting

Models/SingletStandardModel_Z2/SingletStandardModel_Z2.py
@@ -133,9 +133,9 @@ class EffectivePotentialxSM_Z2(EffectivePotential):
 
     def evaluate(self, fields: np.ndarray[float], temperature: float) -> complex:
         #return evaluateHighT(fields, temperature)
-        
         # for Benoit benchmark we don't use high-T approx and no resummation: just Coleman-Weinberg with numerically evaluated thermal 1-loop
 
+        fields = np.asanyarray(fields) 
         v = fields[0] # phi ~ 1/sqrt(2) (0, v)
         x = fields[1] # just S -> S + x 
 
@@ -169,7 +169,9 @@ class EffectivePotentialxSM_Z2(EffectivePotential):
             + self.pressureLO(bosonStuff, fermionStuff, temperature)
             + V0
             + self.V1(bosonStuff, fermionStuff, RGScale) 
-            + self.V1T(bosonStuff, fermionStuff, temperature))
+            + self.V1T(bosonStuff, fermionStuff, temperature)
+        )
+        print(VTotal)
 
         return VTotal
 
@@ -274,6 +276,7 @@ class EffectivePotentialxSM_Z2(EffectivePotential):
 
         # Is this necessary?
         fields = np.asanyarray(fields)
+        T = np.asanyarray(temperature)
         v, x = fields[0,...], fields[1,...]
 
         # TODO: numerical determination of scalar masses from V0
@@ -352,7 +355,7 @@ def main():
 
     model = SingletSM_Z2(inputParameters)
 
-    Tn = 100
+    Tn = 100.
 
     userInput = {
         "Tn" : Tn,
@@ -369,10 +372,10 @@ def main():
     res=manager.hydro.findHydroBoundaries(0.5229)
     print(res)
 
-    M, N = 20, 20
-    manager.initGrid(M, N)
+    # M, N = 20, 20
+    # manager.initGrid(M, N)
 
-    manager.solveWall()
+    # manager.solveWall()
 
     
 

src/WallSpeed/EffectivePotential.py
@@ -112,7 +112,7 @@ class EffectivePotential(ABC):
             
         ## res.x is the minimum location, res.fun is the function value
         # print(res.x)
-        print(res.fun)
+        # print(res.fun)
         return res.x, res.fun
     
     
@@ -170,7 +170,7 @@ class EffectivePotential(ABC):
 
 
     @staticmethod
-    def Jcw(msq, degrees_of_freedom, c, RGScale: float):
+    def Jcw(msq: float, degrees_of_freedom: int, c: float, RGScale: float):
         """
         Coleman-Weinberg potential
 
@@ -265,7 +265,7 @@ class EffectivePotential(ABC):
 
 
     @staticmethod
-    def V1T(bosons, fermions, T):
+    def V1T(bosons, fermions, temperature: float):
         """
         One-loop thermal correction to the effective potential without any temperature expansions.
 
@@ -301,11 +301,18 @@ class EffectivePotential(ABC):
              # the 1e-100 is to avoid divide by zero errors
         T4 = T*T*T*T
         """
-
+        T = np.asanyarray(temperature)
         m2,nb,_ = bosons
-        V = np.sum(nb* WallSpeed.Integrals.Jb(m2/T**2 + 1e-100), axis=-1)
+
+        V = np.zeros_like(T)
+
+        for m2_value, nb_value in zip(m2, nb):
+            V += nb_value * WallSpeed.Integrals.Jb(m2_value / T**2 + 1e-100)
+
         m2,nf = fermions
-        V += np.sum(nf* WallSpeed.Integrals.Jf(m2/T**2 + 1e-100), axis=-1)
+        for m2_value, nf_value in zip(m2, nf):
+            V += nf_value * WallSpeed.Integrals.Jf(m2_value / T**2 + 1e-100)
+
         return V*T**4 / (2*np.pi*np.pi)
 
     