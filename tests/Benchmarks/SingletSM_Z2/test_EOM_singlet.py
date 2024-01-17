f133fe44249828365fd00932fc05238cb3e0bb20
Lauri Niemi <lauri.b.niemi@helsinki.fi>
2024-01-17 13:12:56 +0200

    made EOM test use absolute tolerance instead of relative

--- tests/Benchmarks/SingletSM_Z2/test_EOM_singlet.py
+++ tests/Benchmarks/SingletSM_Z2/test_EOM_singlet.py
@@ -19,4 +19,6 @@ def test_equilibriumEOM_singlet(singletBenchmarkEOM_equilibrium: Tuple[WallGo.EO
     vwLoop, _ = eom.findWallVelocityMinimizeAction()
     vwLTE = BM.expectedResults["vwLTE"]
 
-    assert(vwLoop == pytest.approx(vwLTE, rel = 1e-2))
+    ## Currently the wall velocity solver in EOM has hardcoded absolute tolerance of 1e-3. So no point testing for more precision than that
+
+    assert(vwLoop == pytest.approx(vwLTE, abs = 1e-3))
