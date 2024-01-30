4e225c7b47a60d8b0588f84838c00201a1964eaa
Benoit Laurent <benoitlaurent@MacBook-Air-de-Benoit-2.local>
2024-01-30 10:22:12 +0100

    Changed the way CollisionArray changes the basis

--- src/WallGo/CollisionArray.py
+++ src/WallGo/CollisionArray.py
@@ -39,14 +39,22 @@ class CollisionArray:
         self.basis = basis
         self.particle1 = particle1
         self.particle2= particle2
+        
+        # Load the collision file
         self.loadFile(collisionFilename)
+        
+        # Change the basis
+        self.collisionFilePoly.changeBasis(("Cardinal", "Cardinal", basis, basis), inverseTranspose=True)
+        
+        # Extract the collision array
+        self.collisionArray = self.collisionFilePoly.coefficients
         
     def __getitem__(self, key):
         return self.collisionArray[key]
         
     def loadFile(self, filename: str):
         """
-        Load the collision array and transform it to be used by Boltzmann.
+        Load the collision array and store it in a Polynomial object.
 
         Parameters
         ----------
@@ -61,77 +69,41 @@ class CollisionArray:
         try:
             with h5py.File(filename, "r") as file:
                 metadata = file["metadata"]
-                basisSize = metadata.attrs["Basis Size"]
-                basisType = codecs.decode(
+                basisSizeFile = metadata.attrs["Basis Size"]
+                basisTypeFile = codecs.decode(
                     metadata.attrs["Basis Type"], 'unicode_escape',
                 )
-                CollisionArray.__checkBasis(basisType)
+                CollisionArray.__checkBasis(basisTypeFile)
 
                 # LN: currently the dataset names are of form
                 # "particle1, particle2". Here it's just "top, top" for now.
                 datasetName = self.particle1.name + ", " + self.particle2.name
-                collisionArray = np.array(file[datasetName][:])
+                collisionFileArray = np.array(file[datasetName][:])
         except FileNotFoundError:
             print("CollisionArray error: %s not found" % filename)
             raise
             
-        # Need to make sure basisSize has the same meaning as self.N!!!
-        assert basisSize <= self.N, f"CollisionArray error: Basis size of {filename} too small to generate a collision array of size N = {self.N}."
-
         # converting between conventions
-        collisionArray = np.transpose(
-            np.flip(collisionArray, (2, 3)),
+        collisionFileArray = np.transpose(
+            np.flip(collisionFileArray, (2, 3)),
             (2, 3, 0, 1),
         )
         
-        # Make sure collisionArray is in the right basis
-        self.collisionArray = self.__changeBasis(collisionArray, basisType, self.basis)
-        
-    def interpolateArray(self):
-        pass
-        
-    def __changeBasis(self, collisionArray: np.ndarray, oldBasis: str, newBasis: str):
-        """
-        Transforms the basis of collisionArray to 'Cardinal'.
-
-        Parameters
-        ----------
-        collisionArray : np.ndarray
-            Array containing the collision term.
-        oldBasis : str
-            Basis of the current collisionArray.
-        newBasis : str
-            Desired basis for collisionArray.
-
-        Returns
-        -------
-        None.
-
-        """
-        
-        CollisionArray.__checkBasis(oldBasis)
-        CollisionArray.__checkBasis(newBasis)
-        
         # Create a Grid object to be used by Polynomial
-        N = collisionArray.shape[0]+1
-        grid = Grid(N,N,1,1)
-        
+        gridFile = Grid(basisSizeFile, basisSizeFile, 1, 1)
+            
         # Create the Polynomial object 
-        collisionPoly = Polynomial(
-            collisionArray,
-            grid,
-            ("Cardinal", "Cardinal", oldBasis, oldBasis),
+        self.collisionFilePoly = Polynomial(
+            collisionFileArray,
+            gridFile,
+            ("Cardinal", "Cardinal", basisTypeFile, basisTypeFile),
             ("pz", "pp", "pz", "pp"),
             False,
-        )
+        ) 
         
-        # Change the basis
-        collisionPoly.changeBasis(("Cardinal", "Cardinal", newBasis, newBasis), inverseTranspose=True)
-        
-        return collisionPoly.coefficients
-        
-        
-        
+    def interpolateArray(self, newBasisSize):
+        assert newBasisSize <= self.collisionFilePoly.grid.N, f"CollisionArray error: Basis size too small to generate a collision array of size N = {newBasisSize}."        
+          
     def __checkBasis(basis: str):
         """
         Check that basis is reckognised
