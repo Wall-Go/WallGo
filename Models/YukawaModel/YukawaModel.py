2396c0370e9c0c6ce8b3837a78987aafaa8744fe
Hirvonen <76170033+joonashir@users.noreply.github.com>
2024-02-15 18:57:21 +0200

    Correct input parameters, should be a strong transition

--- Models/YukawaModel/YukawaModel.py
+++ Models/YukawaModel/YukawaModel.py
@@ -397,18 +397,10 @@ def main():
 
     ## QFT model input. Some of these are probably not intended to change, like gauge masses. Could hardcode those directly in the class.
     inputParameters = {
-        #"RGScale" : 91.1876,
-        "RGScale" : 125., # <- Benoit benchmark
-        "v0" : 246.0,
-        "MW" : 80.379,
-        "MZ" : 91.1876,
-        "Mt" : 173.0,
-        "g3" : 1.2279920495357861,
-        # scalar specific, choose Benoit benchmark values
-        "mh1" : 125.0,
-        "mh2" : 120.0,
-        "a2" : 0.9,
-        "b4" : 1.0
+        "m_b" : 1,
+        "g" : -0.89,
+        "lambda" : 0.056,
+        "y" : 0.23
     }
 
     model = SingletSM_Z2(inputParameters)
