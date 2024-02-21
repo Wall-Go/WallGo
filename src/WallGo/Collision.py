29124caa39d757603d7e66e0ecdf0300957c0e1e
Lauri Niemi <lauri.b.niemi@helsinki.fi>
2024-02-21 16:28:07 +0200

    print collision load error

--- src/WallGo/Collision.py
+++ src/WallGo/Collision.py
@@ -21,8 +21,9 @@ def loadCollisionModule():
         collisionModule = importlib.import_module(moduleName)
         print("Loaded module [%s]" % moduleName)
 
-    except ImportError:
+    except ImportError as error:
         print("Warning: Failed to load [%s]. Using read-only mode for collision integrals." % moduleName)
         print("Computation of new collision integrals will NOT be possible.")
+        print(error)
 
     return collisionModule
