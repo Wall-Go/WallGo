d6ce946b63fcb56d0e40bdb1cf8cdeb38c03ca87
Lauri Niemi <lauri.b.niemi@helsinki.fi>
2024-02-22 13:17:33 +0200

    explicitly specify Python version in CMake

--- src/WallGo/Collision.py
+++ src/WallGo/Collision.py
@@ -24,6 +24,5 @@ def loadCollisionModule():
     except ImportError as error:
         print("Warning: Failed to load [%s]. Using read-only mode for collision integrals." % moduleName)
         print("Computation of new collision integrals will NOT be possible.")
-        print(error)
 
     return collisionModule
