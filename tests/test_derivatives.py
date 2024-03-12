925791e9115b06e8ebe34d23ae8371329baed3bd
Oliver Gould <oliver.gould@nottingham.ac.uk>
2024-03-12 17:42:09 +0000

    Made test_derivatives.py a bit more informative

--- tests/test_derivatives.py
+++ tests/test_derivatives.py
@@ -2,14 +2,66 @@ import pytest
 import numpy as np
 import WallGo
 
-def test_derivative():
-    xs = np.linspace(-10,10,100000)
-    
-    f = lambda x: x*np.sin(x)
-    dfdx = lambda x: np.sin(x)+x*np.cos(x)
-    d2fdx2 = lambda x: 2*np.cos(x)-x*np.sin(x)
-    
-    np.testing.assert_allclose(dfdx(xs), WallGo.helpers.derivative(f, xs, 1, 2, (-10,10)),atol=1e-5)
-    np.testing.assert_allclose(dfdx(xs), WallGo.helpers.derivative(f, xs, 1, 4, (-10,10)),atol=1e-40)
-    np.testing.assert_allclose(d2fdx2(xs), WallGo.helpers.derivative(f, xs, 2, 2, (-20,20)),atol=1e-6)
-    np.testing.assert_allclose(d2fdx2(xs), WallGo.helpers.derivative(f, xs, 2, 4, (-10,10)),atol=1e-7)
+
+def f_analytic(x):
+    # a simple function to test derivatives
+    return x * np.sin(x)
+
+
+def dfdx_analytic(x):
+    # the first derivative, analytically
+    return np.sin(x) + x * np.cos(x)
+
+
+def d2fdx2_analytic(x):
+    # the second derivative, analytically
+    return 2 * np.cos(x) - x * np.sin(x)
+
+
+@pytest.fixture
+def xRange():
+    # the values of x where to test the derivative
+    return np.linspace(-10, 10, num=100)
+
+
+@pytest.mark.parametrize(
+    "n, order, bounded, aTol",
+    [
+        (1, 2, False, 1e-6),
+        (1, 2, True, 1e-3),
+        (2, 2, False, 1e-6),
+        (2, 2, True, 1e-2),
+        (1, 4, False, 1e-12),
+        (1, 4, True, 1e-12),
+        (2, 4, False, 1e-12),
+        (2, 4, True, 1e-12),
+    ]
+)
+def test_derivative(
+    xRange, n: int, order: int, bounded: bool, aTol: float,
+):
+    """
+    Tests accuracy of derivative function
+    """
+
+    # bounds?
+    if bounded:
+        bounds = (min(xRange), max(xRange))
+    else:
+        bounds = None
+
+    # expected result
+    if n == 1:
+        deriv_analytic = dfdx_analytic(xRange)
+    elif n == 2:
+        deriv_analytic = d2fdx2_analytic(xRange)
+    else:
+        raise WallGo.WallGoError(
+            f"derivative function supports n=1,2, not {n=}"
+        )
+
+    # testing first derivatives
+    deriv_WallGo = WallGo.helpers.derivative(
+        f_analytic, xRange, n=n, order=order, bounds=bounds
+    )
+    np.testing.assert_allclose(deriv_WallGo, deriv_analytic, atol=aTol)
