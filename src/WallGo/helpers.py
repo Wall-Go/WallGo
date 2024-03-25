634034fabf4f062cd1b9e278b79bd9d2d244c1c2
Benoit Laurent <benoit.laurent96@gmail.com>
2024-03-25 19:57:39 +0100

    Created gradient function

--- src/WallGo/helpers.py
+++ src/WallGo/helpers.py
@@ -52,7 +52,7 @@ def derivative(f, x, n=1, order=4, bounds=None, epsilon=1e-16, scale=1.0, dx=Non
         The number of derivatives to take. Can be 0, 1, 2. The default is 1.
     order : int, optional
         The accuracy order of the scheme. Errors are of order
-        :math:`\mathcal{O}({\rm d}x^{\text{order}+1})`. Can be 2 or 4. Note that 
+        :math:`\mathcal{O}({\rm d}x^{\text{order}/(\text{order+n})})`. Can be 2 or 4. Note that 
         the order at the endpoints is reduced by 1 as it would require 
         more function evaluations to keep the same order. The default is 4.
     bounds : tuple or None, optional
@@ -128,7 +128,60 @@ def derivative(f, x, n=1, order=4, bounds=None, epsilon=1e-16, scale=1.0, dx=Non
     return np.sum(coeff.reshape(coeff.shape+(fxShapeLength-coeffShapeLength)*(1,)) * f(pos, *args), axis=0)
         
     
+def gradient(f, x, order=4, epsilon=1e-16, scale=1.0, dx=None, args=None):
+    r"""Computes the gradient of a callable function. Use the epsilon
+    and scale parameters to estimate the optimal value of dx, if the latter is
+    not provided. 
     
+
+    Parameters
+    ----------
+    f : function
+        Function to differentiate. Should take an array as argument 
+        and return an array.
+    x : array-like
+        The position at which to evaluate the derivative. The size of the last
+        axis must correspond to the number of variables on which f depends.
+    order : int, optional
+        The accuracy order of the scheme. Errors are of order
+        :math:`\mathcal{O}({\rm d}x^{\text{order}/(\text{order}+1)})`. Can be 2 or 4. Note that 
+        the order at the endpoints is reduced by 1 as it would require 
+        more function evaluations to keep the same order. The default is 4.
+    epsilon : float, optional
+        Fractional accuracy at which f can be evaluated. If f is a simple 
+        function, should be close to the machine precision. Default is 1e-16.
+    scale : float, optional 
+        Typical scale at which f(x) change by order 1. Default is 1.
+    dx : float or None, optional
+        The magnitude of finite differences. If None, use epsilon and scale to
+        estimate the optimal dx. Default is None.
+    args: list, optional
+        List of other fixed arguments passed to the function :math:`f`.
+
+    Returns
+    -------
+    res : float
+        The value of the derivative of :py:data:`f` evaluated at :py:data:`x`.
+
+    """
+    x = np.asarray(x)
+    
+    if args is None:
+        args = []
+    
+    assert order == 2 or order == 4, "Gradient error: order must be 2 or 4."
+    
+    # If dx is not provided, we estimate it from scale and epsilon by minimizing 
+    # the total error ~ epsilon/dx**n + dx**order.
+    if dx is None:
+        assert isinstance(epsilon, float), "Gradient error: epsilon must be a float."
+        assert isinstance(scale, float), "Gradient error: scale must be a float."
+        dx = scale * epsilon**(1/(1+order))
+    
+    # This step increases greatly the accuracy because it makes sure (x + dx) - x
+    # is exactly equal to dx (no precision error).
+    temp = x + dx
+    dx = temp - x
     
 
 
