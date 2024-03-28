Fixed a lot of bugs and changed how the gradient and hessian are computed in EffectivePotential

src/WallGo/EffectivePotential.py
@@ -52,11 +52,9 @@ class EffectivePotential(ABC):
         self.modelParameters = modelParameters
         self.fieldCount = fieldCount
         
-        # Intitial guess for fieldScale. Will be overwritten by self.setScales.
+        # Intitializes fieldScale. Will be overriden by self.setScales.
         self.fieldScale = np.ones(fieldCount)
-
-        ## Used for derivatives. TODO read from config file probably
-        self.dPhi = 0.1  ## field difference
+        self.__combinedScales = np.append(self.fieldScale, self.temperatureScale)
 
 
     @abstractmethod
@@ -89,6 +87,7 @@ class EffectivePotential(ABC):
         else:
             self.fieldScale = np.asanyarray(fieldScale)
             assert self.fieldScale.size == self.fieldCount, "EffectivePotential error: fieldScale must have a size of self.fieldCount."
+        self.__combinedScales = np.append(self.fieldScale, self.temperatureScale)
 
     def findLocalMinimum(self, initialGuess: Fields, temperature: npt.ArrayLike, tol: float = None) -> Tuple[Fields, npt.ArrayLike]:
         """
@@ -139,6 +138,19 @@ class EffectivePotential(ABC):
 
         ## Need to cast the field location
         return Fields.CastFromNumpy(resLocation), resValue
+    
+    def __wrapperPotential(self, X):
+        fields = Fields(X[...,:-1])
+        temperature = X[...,-1]
+        return self.evaluate(fields, temperature)
+    
+    def __combineInputs(self, fields, temperature):
+        shape = list(fields.shape)
+        shape[-1] += 1
+        combinedInput = np.empty(shape)
+        combinedInput[...,:-1] = fields
+        combinedInput[...,-1] = temperature
+        return combinedInput
 
     def derivT(self, fields: Fields, temperature: npt.ArrayLike):
         """Calculate derivative of (real part of) the effective potential with
@@ -186,28 +198,9 @@ class EffectivePotential(ABC):
             Field derivatives of the potential, one Fields object for each
             temperature. They are of Fields type since the shapes match nicely.
         """
-        ## LN: had trouble setting the offset because numpy tried to use it integer and rounded it to 0. So paranoid dtype everywhere here
-
-        res = np.empty_like((fields.T*temperature).T, dtype=float)
-
-        for idx in range(fields.NumFields()):
-            fieldsOffset = np.zeros_like(fields, dtype=float)
-            fieldsOffset.SetField(
-                idx, np.full(fields.NumPoints(), self.dPhi, dtype=float)
-            )
-
-            # O(dPhi^4) accurate, central finite difference scheme
-            dV = (
-                -self.evaluate(fields + 2 * fieldsOffset, temperature)
-                + 8 * self.evaluate(fields + fieldsOffset, temperature)
-                - 8 * self.evaluate(fields - fieldsOffset, temperature)
-                + self.evaluate(fields - 2 * fieldsOffset, temperature)
-            )
-            dVdphi = dV / (12 * self.dPhi)
-
-            res.SetField(idx, dVdphi)
-
-        return res
+        
+        return gradient(self.__wrapperPotential, self.__combineInputs(fields, temperature), epsilon=self.effectivePotentialError, 
+                        scale=self.__combinedScales, axis=np.arange(self.fieldCount).tolist())
 
     def deriv2FieldT(self, fields: Fields, temperature: npt.ArrayLike):
         r""" Computes :math:`d^2V/(d\text{Field} dT)`.
@@ -225,32 +218,9 @@ class EffectivePotential(ABC):
             Field derivatives of the potential, one Fields object for each
             temperature. They are of Fields type since the shapes match nicely.
         """
-        # if len(fields.shape) == 1:
-        #     # HACK! Not sure how best to deal with this edge case, which
-        #     # arises due to how scipyint.RK45 is initialised
-        #     fields = Fields((fields))
         
-        # res = derivative(
-        #     lambda T: self.derivField(fields, T),
-        #     temperature,
-        #     n=1,
-        #     order=4,
-        #     epsilon=self.effectivePotentialError,
-        #     scale=self.temperatureScale,
-        #     bounds=(0,np.inf),
-        # )
-        def wrapperPotential(X):
-            fieldsWrapper = Fields(X[...,:-1])
-            T = X[...,-1]
-            return self.evaluate(fieldsWrapper, T)
-        
-        shape = list(fields.shape)
-        shape[-1] += 1
-        hessianInput = np.empty(shape)
-        hessianInput[...,:-1] = fields
-        hessianInput[...,-1] = temperature
-        
-        res = hessian(wrapperPotential,hessianInput, epsilon=self.effectivePotentialError, scale=np.append(self.fieldScale, self.temperatureScale), axis=-1)[...,:-1]
+        res = hessian(self.__wrapperPotential, self.__combineInputs(fields, temperature), epsilon=self.effectivePotentialError, 
+                      scale=self.__combinedScales, xAxis=np.arange(self.fieldCount).tolist(), yAxis=-1)[...,0]
         
         return res
 
@@ -270,80 +240,39 @@ class EffectivePotential(ABC):
             Field Hessian of the potential. For each temperature, this is
             a matrix of the same size as Fields.
         """
-        if len(fields.shape) == 1:
-            # HACK! Not sure how best to deal with this edge case, which
-            # arises due to how scipyint.RK45 is initialised
-            fields = Fields((fields))
-        shapeRes = (fields.NumPoints(), fields.NumFields(), fields.NumFields())
-        res = np.empty(shapeRes, dtype=float)
+        
+        axis = np.arange(self.fieldCount).tolist()
+        return hessian(self.__wrapperPotential, self.__combineInputs(fields, temperature), epsilon=self.effectivePotentialError, 
+                       scale=self.__combinedScales, xAxis=axis, yAxis=axis)
+    
+    def allSecondDerivatives(self, fields: Fields, temperature: npt.ArrayLike):
+        r""" Computes :math:`d^2V/(d\text{Field}^2)`, :math:`d^2V/(d\text{Field} dT)` 
+        and :math:`d^2V/(dT^2)` at the ssame time. This function is more efficient
+        than calling the other functions one at a time.
 
-        # OG: This is all a bit of a mess, and should probably be rewritten
-        # more algorithmically.
+        Parameters
+        ----------
+        fields : Fields
+            The background field values (e.g.: Higgs, singlet)
+        temperature : array_like
+            The temperature
 
-        for idx in range(fields.NumFields()):
-            fieldsOffsetX = np.zeros_like(fields, dtype=float)
-            fieldsOffsetX.SetField(
-                idx, np.full(fields.NumPoints(), self.dPhi, dtype=float)
-            )
-            for idy in range(idx, fields.NumFields()):
-                fieldsOffsetY = np.zeros_like(fields, dtype=float)
-                if idy == idx:
-                    """# O(dPhi^2) accurate, central finite difference scheme
-                    dV = (
-                        self.evaluate(fields + fieldsOffsetX, temperature)
-                        - 2 * self.evaluate(fields, temperature)
-                        + self.evaluate(fields - fieldsOffsetX, temperature)
-                    )
-                    res[..., idx, idy] = dV / self.dPhi ** 2"""
-                    # O(dPhi^4) accurate, central finite difference scheme
-                    dV = (
-                        -self.evaluate(fields + 2 * fieldsOffsetX, temperature)
-                        + 16 * self.evaluate(fields + fieldsOffsetX, temperature)
-                        - 30 * self.evaluate(fields, temperature)
-                        + 16 * self.evaluate(fields - fieldsOffsetX, temperature)
-                        - self.evaluate(fields - 2 * fieldsOffsetX, temperature)
-                    )
-                    res[..., idx, idy] = dV / (12 * self.dPhi ** 2)
-                else:
-                    fieldsOffsetY.SetField(
-                        idy, np.full(fields.NumPoints(), self.dPhi, dtype=float)
-                    )
-                    """# O(dPhi^2) accurate, central finite difference scheme
-                    dV = (
-                        self.evaluate(fields + fieldsOffsetX + fieldsOffsetY, temperature)
-                        - self.evaluate(fields + fieldsOffsetX - fieldsOffsetY, temperature)
-                        - self.evaluate(fields - fieldsOffsetX + fieldsOffsetY, temperature)
-                        + self.evaluate(fields - fieldsOffsetX - fieldsOffsetY, temperature)
-                    )
-                    res[..., idx, idy] = dV / (4 * self.dPhi ** 2)
-                    res[..., idy, idx] = res[..., idx, idy]"""
-                    # O(dPhi^4) accurate, central finite difference scheme
-                    dV = (
-                        self.evaluate(fields + 2 * fieldsOffsetX + 2 * fieldsOffsetY, temperature)
-                        - self.evaluate(fields - 2 * fieldsOffsetX + 2 * fieldsOffsetY, temperature)
-                        - self.evaluate(fields + 2 * fieldsOffsetX - 2 * fieldsOffsetY, temperature)
-                        + self.evaluate(fields - 2 * fieldsOffsetX - 2 * fieldsOffsetY, temperature)
-                    )
-                    dV += 8 * (
-                        -self.evaluate(fields + 2 * fieldsOffsetX + fieldsOffsetY, temperature)
-                        + self.evaluate(fields - 2 * fieldsOffsetX + fieldsOffsetY, temperature)
-                        + self.evaluate(fields + 2 * fieldsOffsetX - fieldsOffsetY, temperature)
-                        - self.evaluate(fields - 2 * fieldsOffsetX - fieldsOffsetY, temperature)
-                        - self.evaluate(fields + fieldsOffsetX + 2 * fieldsOffsetY, temperature)
-                        + self.evaluate(fields - fieldsOffsetX + 2 * fieldsOffsetY, temperature)
-                        + self.evaluate(fields + fieldsOffsetX - 2 * fieldsOffsetY, temperature)
-                        - self.evaluate(fields - fieldsOffsetX - 2 * fieldsOffsetY, temperature)
-                    )
-                    dV += 64 * (
-                        self.evaluate(fields + fieldsOffsetX + fieldsOffsetY, temperature)
-                        - self.evaluate(fields - fieldsOffsetX + fieldsOffsetY, temperature)
-                        - self.evaluate(fields + fieldsOffsetX - fieldsOffsetY, temperature)
-                        + self.evaluate(fields - fieldsOffsetX - fieldsOffsetY, temperature)
-                    )
-                    res[..., idx, idy] = dV / (144 * self.dPhi ** 2)
-                    res[..., idy, idx] = res[..., idx, idy]
-
-        if len(res.shape) == 3 and res.shape[0] == 1:
-            # HACK! SHOULD DO THIS MORE TIDILY
-            return res[0]
-        return res
+        Returns
+        ----------
+        d2VdField2 : list[Fields]
+            Field Hessian of the potential. For each temperature, this is
+            a matrix of the same size as Fields.
+        d2fdFielddT : list[Fields]
+            Field derivatives of the potential, one Fields object for each
+            temperature. They are of Fields type since the shapes match nicely.
+        d2VdT2 : array-like
+            Temperature second derivative of the potential.
+        """
+        
+        res = hessian(self.__wrapperPotential, self.__combineInputs(fields, temperature), epsilon=self.effectivePotentialError, scale=self.__combinedScales)
+        
+        hess = res[...,:-1,:-1]
+        dgraddT = res[...,-1,:-1]
+        d2VdT2 = res[...,-1,-1]
+        
+        return hess, dgraddT, d2VdT2

src/WallGo/helpers.py
@@ -129,7 +129,7 @@ def derivative(f, x, n=1, order=4, bounds=None, epsilon=1e-16, scale=1.0, dx=Non
     return np.sum(coeff.reshape(coeff.shape+(fxShapeLength-coeffShapeLength)*(1,)) * f(pos, *args), axis=0)
         
     
-def gradient(f, x, order=4, epsilon=1e-16, scale=1.0, dx=None, args=None):
+def gradient(f, x, order=4, epsilon=1e-16, scale=1.0, dx=None, axis=None, args=None):
     r"""Computes the gradient of a callable function. Use the epsilon
     and scale parameters to estimate the optimal value of dx, if the latter is
     not provided. 
@@ -158,6 +158,9 @@ def gradient(f, x, order=4, epsilon=1e-16, scale=1.0, dx=None, args=None):
         The magnitude of finite differences. Can be an array, in which case 
         each element corresponds to the dx of a different variable.If None, use 
         epsilon and scale to estimate the optimal dx. Default is None.
+    axis : list, int or None, optional
+        Element of the gradient to return. If None, returns the whole gradient.
+        Default is None.
     args: list, optional
         List of other fixed arguments passed to the function :math:`f`.
 
@@ -172,6 +175,13 @@ def gradient(f, x, order=4, epsilon=1e-16, scale=1.0, dx=None, args=None):
     
     if args is None:
         args = []
+        
+    if isinstance(axis, int):
+        axis = [axis]
+    elif axis is None:
+        axis = np.arange(nbrVariables).tolist()
+    for i in axis:
+        assert -nbrVariables <= i < nbrVariables, "Gradient error: axis must be between -nbrVariables and nbrVariables-1 or None."
     
     assert order == 2 or order == 4, "Gradient error: order must be 2 or 4."
     
@@ -197,15 +207,15 @@ def gradient(f, x, order=4, epsilon=1e-16, scale=1.0, dx=None, args=None):
     temp = x + dx
     dx = temp - x
     
-    pos = np.expand_dims(x, (-3,-2)) + FIRST_DERIV_POS[str(order)][0,:,None,None]*np.identity(nbrVariables)*np.expand_dims(dx, (-3,-2)) 
+    pos = np.expand_dims(x, (-3,-2)) + FIRST_DERIV_POS[str(order)][0,:,None,None]*np.identity(nbrVariables)[axis,:]*np.expand_dims(dx, (-3,-2)) 
     shape = pos.shape[:-1]
     pos = pos.reshape((int(pos.size/nbrVariables), nbrVariables))
-    coeff = FIRST_DERIV_COEFF[str(order)][0,:,None]/np.expand_dims(dx, -2) 
+    coeff = FIRST_DERIV_COEFF[str(order)][0,:,None]/np.expand_dims(dx[...,axis], -2) 
         
     fEvaluation = f(pos, *args).reshape(shape)
     return np.sum(coeff*fEvaluation, axis=-2)
 
-def hessian(f, x, order=4, epsilon=1e-16, scale=1.0, dx=None, axis=None, args=None):
+def hessian(f, x, order=4, epsilon=1e-16, scale=1.0, dx=None, xAxis=None, yAxis=None, args=None):
     r"""Computes the hessian of a callable function. Use the epsilon
     and scale parameters to estimate the optimal value of dx, if the latter is
     not provided. 
@@ -231,8 +241,11 @@ def hessian(f, x, order=4, epsilon=1e-16, scale=1.0, dx=None, axis=None, args=No
     dx : float or None, optional
         The magnitude of finite differences. If None, use epsilon and scale to
         estimate the optimal dx. Default is None.
-    axis : int or None, optional
-        Line of the hessian matrix to return. If None, returns the whole matrix.
+    xAxis : list, int or None, optional
+        Lines of the hessian matrix to return. If None, returns all the lines.
+        Default is None.
+    yAxis : list, int or None, optional
+        Columns of the hessian matrix to return. If None, returns all the columns.
         Default is None.
     args: list, optional
         List of other fixed arguments passed to the function :math:`f`.
@@ -249,10 +262,18 @@ def hessian(f, x, order=4, epsilon=1e-16, scale=1.0, dx=None, axis=None, args=No
     if args is None:
         args = []
         
-    if isinstance(axis, int):
-        assert -nbrVariables <= axis < nbrVariables, "Hessian error: axis must be between -nbrVariables and nbrVariables-1 or None."
-    else:
-        assert axis is None, "Hessian error: axis must be an int or None."
+    if isinstance(xAxis, int):
+        xAxis = [xAxis]
+    elif xAxis is None:
+        xAxis = np.arange(nbrVariables).tolist()
+    for i in xAxis:
+        assert -nbrVariables <= i < nbrVariables, "Hessian error: axis must be between -nbrVariables and nbrVariables-1 or None."
+    if isinstance(yAxis, int):
+        yAxis = [yAxis]
+    elif yAxis is None:
+        yAxis = np.arange(nbrVariables).tolist()
+    for i in yAxis:
+        assert -nbrVariables <= i < nbrVariables, "Hessian error: axis must be between -nbrVariables and nbrVariables-1 or None."
     
     assert order == 2 or order == 4, "Hessian error: order must be 2 or 4."
     
@@ -278,24 +299,14 @@ def hessian(f, x, order=4, epsilon=1e-16, scale=1.0, dx=None, axis=None, args=No
     temp = x + dx
     dx = temp - x
     
-    if axis is None:
-        pos = (np.expand_dims(x, (-4,-3,-2)) 
-               + HESSIAN_POS[str(order)][0,:,None,None,None]*np.identity(nbrVariables)[:,None,:]*np.expand_dims(dx, (-4,-3,-2))  
-               + HESSIAN_POS[str(order)][1,:,None,None,None]*np.identity(nbrVariables)[None,:,:]*np.expand_dims(dx, (-4,-3,-2)) )
-        shape = pos.shape[:-1]
-        pos = pos.reshape((int(pos.size/nbrVariables), nbrVariables))
-        coeff = HESSIAN_COEFF[str(order)][:,None,None]/(np.expand_dims(dx, (-3,-2))*np.expand_dims(dx, (-3,-1)))
-        fEvaluation = f(pos, *args).reshape(shape)
-        return np.sum(coeff*fEvaluation, axis=-3)
-    else:
-        pos = (np.expand_dims(x, (-3,-2)) 
-               + HESSIAN_POS[str(order)][0,:,None,None]*np.identity(nbrVariables)[:,:]*np.expand_dims(dx, (-3,-2))  
-               + HESSIAN_POS[str(order)][1,:,None,None]*np.identity(nbrVariables)[None,axis,:]*np.expand_dims(dx, (-3,-2)) )
-        shape = pos.shape[:-1]
-        pos = pos.reshape((int(pos.size/nbrVariables), nbrVariables))
-        coeff = HESSIAN_COEFF[str(order)][:,None]/(np.expand_dims(dx[...,axis], (-2,-1))*np.expand_dims(dx, -2))
-        fEvaluation = f(pos, *args).reshape(shape)
-        return np.sum(coeff*fEvaluation, axis=-2)
+    pos = (np.expand_dims(x, (-4,-3,-2)) 
+           + HESSIAN_POS[str(order)][0,:,None,None,None]*np.identity(nbrVariables)[xAxis,None,:]*np.expand_dims(dx, (-4,-3,-2))  
+           + HESSIAN_POS[str(order)][1,:,None,None,None]*np.identity(nbrVariables)[None,yAxis,:]*np.expand_dims(dx, (-4,-3,-2)) )
+    shape = pos.shape[:-1]
+    pos = pos.reshape((int(pos.size/nbrVariables), nbrVariables))
+    coeff = HESSIAN_COEFF[str(order)][:,None,None]/(np.expand_dims(dx[...,yAxis], (-3,-2))*np.expand_dims(dx[...,xAxis], (-3,-1)))
+    fEvaluation = f(pos, *args).reshape(shape)
+    return np.sum(coeff*fEvaluation, axis=-3)
         
     
 

tests/test_derivatives.py
@@ -97,20 +97,30 @@ def test_derivative(
     np.testing.assert_allclose(deriv_WallGo, deriv_analytic, atol=0, rtol=rTol)
     
 @pytest.mark.parametrize(
-    "order, scaleRatio, rTol",
+    "order, scaleRatio, rTol, axis",
     [
-         (2, 1, 1e-8),
-         (4, 1, 1e-10),
-         (2, 100, 1e-8),
-         (4, 100, 1e-10),
+         (2, 1, 1e-8,None),
+         (4, 1, 1e-10,None),
+         (2, 100, 1e-8,None),
+         (4, 100, 1e-10,None),
+         (2, 1, 1e-8,[0]),
+         (4, 1, 1e-10,[0]),
+         (2, 100, 1e-8,[0]),
+         (4, 100, 1e-10,[0]),
+         (2, 1, 1e-8,[1]),
+         (4, 1, 1e-10,[1]),
+         (2, 100, 1e-8,[1]),
+         (4, 100, 1e-10,[1]),
      ]
 )
-def test_gradient(multivariateRange, order: int, scaleRatio: float, rTol: float):
+def test_gradient(multivariateRange, order: int, scaleRatio: float, rTol: float, axis: list):
     """
     Tests accuracy of gradient function
     """
     gradient_analytic = gradient_fMultivariate_analytic(multivariateRange, scaleRatio)
-    gradient_WallGo = WallGo.helpers.gradient(fMultivariate_analytic, multivariateRange, order, 1e-12, [1,scaleRatio], args=(scaleRatio,))
+    if axis is not None:
+        gradient_analytic = gradient_analytic[...,axis]
+    gradient_WallGo = WallGo.helpers.gradient(fMultivariate_analytic, multivariateRange, order, 1e-12, [1,scaleRatio], axis=axis, args=(scaleRatio,))
     np.testing.assert_allclose(gradient_analytic, gradient_WallGo, atol=0, rtol=rTol)
     
 @pytest.mark.parametrize(
@@ -120,14 +130,14 @@ def test_gradient(multivariateRange, order: int, scaleRatio: float, rTol: float)
          (4, 1, 1e-7, None),
          (2, 100, 1e-6, None),
          (4, 100, 1e-7, None),
-         (2, 1, 1e-6, 0),
-         (4, 1, 1e-7, 0),
-         (2, 100, 1e-6, 0),
-         (4, 100, 1e-7, 0),
-         (2, 1, 1e-6, 1),
-         (4, 1, 1e-7, 1),
-         (2, 100, 1e-6, 1),
-         (4, 100, 1e-7, 1),
+         (2, 1, 1e-6, [0]),
+         (4, 1, 1e-7, [0]),
+         (2, 100, 1e-6, [0]),
+         (4, 100, 1e-7, [0]),
+         (2, 1, 1e-6, [1]),
+         (4, 1, 1e-7, [1]),
+         (2, 100, 1e-6, [1]),
+         (4, 100, 1e-7, [1]),
      ]
 )
 def test_hessian(multivariateRange, order: int, scaleRatio: float, rTol: float, axis: int):
@@ -137,5 +147,5 @@ def test_hessian(multivariateRange, order: int, scaleRatio: float, rTol: float, 
     hessian_analytic = hessian_fMultivariate_analytic(multivariateRange, scaleRatio)
     if axis is not None:
         hessian_analytic = hessian_analytic[...,axis]
-    hessian_WallGo = WallGo.helpers.hessian(fMultivariate_analytic, multivariateRange, order, 1e-12, [1,scaleRatio], axis=axis, args=(scaleRatio,))
+    hessian_WallGo = WallGo.helpers.hessian(fMultivariate_analytic, multivariateRange, order, 1e-12, [1,scaleRatio], yAxis=axis, args=(scaleRatio,))
     np.testing.assert_allclose(hessian_analytic, hessian_WallGo, atol=0, rtol=rTol)