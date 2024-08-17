8b54dd58024624241f493625a658c88b847dc020
Oliver Gould <oliver.gould@nottingham.ac.uk>
2024-08-17 13:06:42 +0100

    Simplified FreeEnergyValueType

--- src/WallGo/freeEnergy.py
+++ src/WallGo/freeEnergy.py
@@ -3,44 +3,46 @@ Class for the free energy of a phase.
 """
 
 import math  # for math.ceil
+from dataclasses import dataclass
 import numpy as np
-import numpy.typing as npt
 import scipy.integrate as scipyint
 import scipy.linalg as scipylinalg
-from dataclasses import dataclass
 
+# WallGo imports
 from .InterpolatableFunction import InterpolatableFunction, EExtrapolationType
 from .EffectivePotential import EffectivePotential
 from .Fields import FieldPoint, Fields
 
+
 @dataclass
 class FreeEnergyValueType:
-    # Value of the effective potential at the free-energy minimum
-    veffValue: npt.ArrayLike
-    # Values of background fields at the free-energy minimum
+    r"""
+    Return type for FreeEnergy, holding both :math:`V(x)` and :math:`x`.
+    """
+
+    veffValue: np.ndarray
+    r"""Value of the effective potential (free energy) at a minimum"""
+
     fieldsAtMinimum: Fields
+    r"""Value of the fields at a minimum"""
 
     @staticmethod
-    def fromArray(arr: npt.ArrayLike) -> 'FreeEnergyValueType':
-        """ASSUMES that the last column is Veff value."""
-        # Awkward dimensionality check needed to figure out correct slicing
-        # TODO can we simplify by forcing resultsArray to always be of certain shape?
-        if arr.ndim < 2:
-            values = arr[-1]
-            fields = arr[:-1]
-        
-        else:
-            values = arr[:, -1]
-            fields = arr[:, :-1] 
-            ## ???
-            if len(values) == 1:
-                values = values[0]
+    def fromArray(arr: np.ndarray) -> "FreeEnergyValueType":
+        """Converts array to FreeEnergyValueType.
+        Assumes that the last column is Veff value,
+        and previous columns are field values.
+        """
+        arr = np.asanyarray(arr)
+        values = arr[..., -1]
+        fields = arr[..., :-1]
 
-        return FreeEnergyValueType(veffValue=values, fieldsAtMinimum=Fields.CastFromNumpy(fields))
+        return FreeEnergyValueType(
+            veffValue=values, fieldsAtMinimum=Fields.CastFromNumpy(fields)
+        )
 
-    
+
 class FreeEnergy(InterpolatableFunction):
-    """ Class FreeEnergy: Describes properties of a local effective potential minimum. 
+    """Class describing properties of a phase, a local minimum of an effective potential.
     This is used to keep track of a minimum with respect to the temperature.
     By definition: free energy density of a phase == value of Veff in its local minimum.
     """
@@ -60,12 +62,12 @@ class FreeEnergy(InterpolatableFunction):
         effectivePotential : EffectivePotential
             EffectivePotential object used to compute the free energy.
         startingTemperature: float
-            Temperature at which the interpolation of the effective potential 
+            Temperature at which the interpolation of the effective potential
             starts.
         startingPhaseLocationGuess : Fields
             Approximate position of the phase at startingTemperature.
         initialInterpolationPointCount : int, optional
-            Initial number of points sampled for the interpolation. 
+            Initial number of points sampled for the interpolation.
             The default is 1000.
 
         Returns
@@ -80,36 +82,35 @@ class FreeEnergy(InterpolatableFunction):
         super().__init__(
             bUseAdaptiveInterpolation=adaptiveInterpolation,
             returnValueCount=returnValueCount,
-            initialInterpolationPointCount=initialInterpolationPointCount
+            initialInterpolationPointCount=initialInterpolationPointCount,
         )
         self.setExtrapolationType(EExtrapolationType.ERROR, EExtrapolationType.ERROR)
-        
+
         self.effectivePotential = effectivePotential
         self.startingTemperature = startingTemperature
         # Approx field values where the phase lies at starting temperature
         self.startingPhaseLocationGuess = startingPhaseLocationGuess
 
         # Lowest possible temperature so that the phase is still (meta)stable
-        self.minPossibleTemperature = 0.
+        self.minPossibleTemperature = 0.0
         # Highest possible temperature so that the phase is still (meta)stable
         self.maxPossibleTemperature = np.inf
 
-
-    def evaluate(self, x: npt.ArrayLike, bUseInterpolatedValues=True) -> FreeEnergyValueType:
-        """Evaluate the free energy. Return value is a FreeEnergyValueType dataclass object.
-        """
-        # Implementation returns array, here we just unpack it. Call to parent class needed to handle interpolation logic 
+    def evaluate(
+        self, x: np.ndarray, bUseInterpolatedValues=True
+    ) -> FreeEnergyValueType:
+        """Evaluate the free energy. Return value is a FreeEnergyValueType dataclass object."""
+        # Implementation returns array, here we just unpack it. Call to parent class needed to handle interpolation logic
         resultsArray = super().evaluate(x, bUseInterpolatedValues)
         return FreeEnergyValueType.fromArray(resultsArray)
 
-
-
-    def __call__(self, x: npt.ArrayLike, bUseInterpolatedValues=True) -> FreeEnergyValueType:
+    def __call__(
+        self, x: np.ndarray, bUseInterpolatedValues=True
+    ) -> FreeEnergyValueType:
         """Just calls self.evaluate()"""
         return self.evaluate(x, bUseInterpolatedValues)
-    
 
-    def _functionImplementation(self, temperature: npt.ArrayLike) -> npt.ArrayLike:
+    def _functionImplementation(self, temperature: np.ndarray) -> np.ndarray:
         """
         Internal implementation of free energy computation. You should NOT call this directly!
         Use the __call__() routine instead.
@@ -141,42 +142,46 @@ class FreeEnergy(InterpolatableFunction):
 
         # This is now a 2D array where rows are [f1, f2, ..., Veff]
         return result
-    
 
-    def derivative(self, x: npt.ArrayLike, order: int = 1, bUseInterpolation=True) -> FreeEnergyValueType:
+    def derivative(
+        self, x: np.ndarray, order: int = 1, bUseInterpolation=True
+    ) -> FreeEnergyValueType:
         """Override of InterpolatableFunction.derivative() function. Specifies accuracy based on our internal variables
-        and puts the results in FreeEnergyValueType format. Otherwise similar to the parent function."""
-        resultsArray = super().derivative(x,
-                                  order,
-                                  bUseInterpolation=bUseInterpolation,
-                                  epsilon=self.effectivePotential.effectivePotentialError,
-                                  scale=self.effectivePotential.temperatureScale)
-        
+        and puts the results in FreeEnergyValueType format. Otherwise similar to the parent function.
+        """
+        resultsArray = super().derivative(
+            x,
+            order,
+            bUseInterpolation=bUseInterpolation,
+            epsilon=self.effectivePotential.effectivePotentialError,
+            scale=self.effectivePotential.temperatureScale,
+        )
+
         return FreeEnergyValueType.fromArray(resultsArray)
 
-
     def tracePhaseOld(self, TMin: float, TMax: float, dT: float) -> None:
-        """For now this will always update the interpolation table.
-        """
+        """For now this will always update the interpolation table."""
 
         TMin = max(self.minPossibleTemperature, TMin)
         TMax = min(self.maxPossibleTemperature, TMax)
 
-        numPoints = math.ceil((TMax-TMin) / dT)
+        numPoints = math.ceil((TMax - TMin) / dT)
         if not self.hasInterpolation():
             self.newInterpolationTable(TMin, TMax, numPoints)
         else:
             currentPoints = self.numPoints()
-            self.extendInterpolationTable(TMin, TMax, math.ceil(numPoints / 2), math.ceil(currentPoints / 2))
+            self.extendInterpolationTable(
+                TMin, TMax, math.ceil(numPoints / 2), math.ceil(currentPoints / 2)
+            )
 
         """We should now have interpolation table in range [TMin, TMax]. 
         If not, it suggests that our Veff minimization became invalid beyond some subrange [TMin', TMax']
         ==> Phase became unstable. 
         """
-        if (self.interpolationRangeMax() < TMax):
+        if self.interpolationRangeMax() < TMax:
             self.maxPossibleTemperature = self.interpolationRangeMax()
 
-        if (self.interpolationRangeMin() > TMin):
+        if self.interpolationRangeMin() > TMin:
             self.minPossibleTemperature = self.interpolationRangeMin()
 
     def tracePhase(
@@ -199,7 +204,9 @@ class FreeEnergy(InterpolatableFunction):
         # initial values, should be nice and accurate
         T0 = self.startingTemperature
         phase0, V0 = self.effectivePotential.findLocalMinimum(
-            self.startingPhaseLocationGuess, T0, tol=extraTol,
+            self.startingPhaseLocationGuess,
+            T0,
+            tol=extraTol,
         )
         phase0 = FieldPoint(phase0[0])
 
@@ -208,20 +215,23 @@ class FreeEnergy(InterpolatableFunction):
 
         def ode_function(temperature, field):
             # ode at each temp is a linear matrix equation A*x=b
-            hess,dgraddT,_ = self.effectivePotential.allSecondDerivatives(field, temperature)
+            hess, dgraddT, _ = self.effectivePotential.allSecondDerivatives(
+                field, temperature
+            )
             return scipylinalg.solve(hess, -dgraddT, assume_a="sym")
 
         # finding some sensible mass scales
         ddV_T0 = self.effectivePotential.deriv2Field2(phase0, T0)
         eigs_T0 = np.linalg.eigvalsh(ddV_T0)
-        #mass_scale_T0 = np.mean(eigs_T0)
-        #min_mass_scale = rTol * mass_scale_T0
-        #mass_hierarchy_T0 = min(eigs_T0) / max(eigs_T0)
-        #min_hierarchy = rTol * mass_hierarchy_T0
+        # mass_scale_T0 = np.mean(eigs_T0)
+        # min_mass_scale = rTol * mass_scale_T0
+        # mass_hierarchy_T0 = min(eigs_T0) / max(eigs_T0)
+        # min_hierarchy = rTol * mass_hierarchy_T0
 
         # checking stable phase at initial temperature
-        assert min(eigs_T0) * max(eigs_T0) > 0, \
-            "tracePhase error: unstable at starting temperature"
+        assert (
+            min(eigs_T0) * max(eigs_T0) > 0
+        ), "tracePhase error: unstable at starting temperature"
 
         def spinodal_event(temperature, field):
             if not spinodal:
@@ -262,7 +272,9 @@ class FreeEnergy(InterpolatableFunction):
                     break
                 if paranoid:
                     phaset, VeffT = self.effectivePotential.findLocalMinimum(
-                        Fields((ode.y)), ode.t, tol=rTol,
+                        Fields((ode.y)),
+                        ode.t,
+                        tol=rTol,
                     )
                     ode.y = phaset[0]
                 if spinodal_event(ode.t, ode.y) <= 0:
@@ -271,10 +283,12 @@ class FreeEnergy(InterpolatableFunction):
                 if not paranoid:
                     # check if extremum is still accurate
                     dVt = self.effectivePotential.derivField(Fields((ode.y)), ode.t)
-                    err = np.linalg.norm(dVt) / T0 ** 3
+                    err = np.linalg.norm(dVt) / T0**3
                     if err > rTol:
                         phaset, VeffT = self.effectivePotential.findLocalMinimum(
-                            Fields((ode.y)), ode.t, tol=extraTol,
+                            Fields((ode.y)),
+                            ode.t,
+                            tol=extraTol,
                         )
                         ode.y = phaset[0]
                     else:
@@ -286,7 +300,9 @@ class FreeEnergy(InterpolatableFunction):
                 VeffList = np.append(VeffList, [VeffT], axis=0)
                 # check if step size is still okay to continue
                 if ode.step_size < 1e-16 * T0:
-                    print(f"Step size {ode.step_size} shrunk too small at T={ode.t}, vev={ode.y}")
+                    print(
+                        f"Step size {ode.step_size} shrunk too small at T={ode.t}, vev={ode.y}"
+                    )
                     break
             if direction == 0:
                 # populating results array
@@ -301,8 +317,12 @@ class FreeEnergy(InterpolatableFunction):
                 if len(TList) > 1:
                     # combining up and down integrations
                     TFullList = np.append(np.flip(TList, 0), TFullList, axis=0)
-                    fieldFullList = np.append(np.flip(fieldList, axis=0), fieldFullList, axis=0)
-                    VeffFullList = np.append(np.flip(VeffList, axis=0), VeffFullList, axis=0)
+                    fieldFullList = np.append(
+                        np.flip(fieldList, axis=0), fieldFullList, axis=0
+                    )
+                    VeffFullList = np.append(
+                        np.flip(VeffList, axis=0), VeffFullList, axis=0
+                    )
                 elif len(TFullList) <= 1:
                     # Both up and down lists are too short
                     raise RuntimeError("Failed to trace phase")
@@ -311,8 +331,9 @@ class FreeEnergy(InterpolatableFunction):
         ## HACK! Hard-coded 2*dT, see issue #145
         self.minPossibleTemperature = min(TFullList) + 2 * dT
         self.maxPossibleTemperature = max(TFullList) - 2 * dT
-        assert self.maxPossibleTemperature > self.minPossibleTemperature, \
-            f"Temperature range negative: decrease dT from {dT}"
+        assert (
+            self.maxPossibleTemperature > self.minPossibleTemperature
+        ), f"Temperature range negative: decrease dT from {dT}"
 
         # Now to construct the interpolation
         result = np.concatenate((fieldFullList, VeffFullList), axis=1)
