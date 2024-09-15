e1efd8683f319fd7de7f1f063461c3e49d16df41
Jorinde van de Vis <jorindevandevis@outlook.com>
2024-09-15 12:57:10 +0200

    Renamed config and added docstrings to all functions

---
+++ src/WallGo/config.py
@@ -0,0 +1,135 @@
+"""
+Class that manages configuration variables for WallGo.
+"""
+import configparser
+import io
+
+class Config:
+    """
+    This class is essentially a wrapper around ConfigParser. Accessing variables
+    works as with ConfigParser: config.get("Section", "someKey")
+    """
+
+    configParser: configparser.ConfigParser
+
+    def __init__(self):
+
+        self.config = configparser.ConfigParser()
+        self.config.optionxform = str # preserve case
+
+
+    def readINI(self, filePath: str):
+        """
+        Reads a configuration file.
+
+        Parameters
+        ----------
+        filePath : str
+            Path to the configuration file.
+
+        Returns
+        -------
+        """
+        self.config.read(filePath)
+
+
+    def get(self, section: str, key: str) -> any:
+        """
+        Returns a variable from the configuration.
+
+        Parameters
+        ----------
+        section : str
+            The section from which to obtain the variable.
+        key : str
+            The key of the variable.
+
+        Returns
+        -------
+        variable: any
+            The configuration variable.
+        """
+        return self.config.get(section, key)
+
+    def getint(self, section: str, key: str) -> int:
+        """
+        Returns an integer variable from the configuration.
+
+        Parameters
+        ----------
+        section : str
+            The section from which to obtain the variable.
+        key : str
+            The key of the variable.
+
+        Returns
+        -------
+        variable: int
+            The configuration variable.
+        """
+        return self.config.getint(section, key)
+
+    def getfloat(self, section: str, key: str) -> float:
+        """
+        Returns a float variable from the configuration.
+
+        Parameters
+        ----------
+        section : str
+            The section from which to obtain the variable.
+        key : str
+            The key of the variable.
+
+        Returns
+        -------
+        variable: float
+            The configuration variable.
+        """
+        return self.config.getfloat(section, key)
+
+    def set(self, section: str, key: str, value: str):
+        """
+        Modifies a variable in the configuration
+
+        Parameters
+        ----------
+        section : str
+            The section under which the variable is listed.
+        key : str
+            The key of the variable.
+        value : str
+            The new value of the variable.
+
+        Returns
+        -------
+        """
+        self.config.set(section, key, value)
+
+    def getboolean(self, section: str, key: str) -> bool:
+        """
+        Returns a boolean variable from the configuration.
+
+        Parameters
+        ----------
+        section : str
+            The section from which to obtain the variable.
+        key : str
+            The key of the variable.
+
+        Returns
+        -------
+        variable: bool
+            The configuration variable.
+        """
+        return self.config.getboolean(section, key)
+
+    def __str__(self) -> str:
+        """
+        Prints the content in similar format as an .ini file would be.
+        """
+
+        ## Do magic by writing the contents into a file-like string buffer
+        buffer = io.StringIO()
+        self.config.write(buffer)
+
+        return buffer.getvalue()
