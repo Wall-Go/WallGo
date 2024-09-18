81ca276be07743753ea0fe8630044a5a45435fef
Philipp Schicho <schicho@icloud.com>
2024-09-18 15:41:11 +0200

    give option for different systems

--- src/WallGo/mathematicaHelpers.py
+++ src/WallGo/mathematicaHelpers.py
@@ -1,22 +1,30 @@
-import subprocess
 import pathlib
+import subprocess
+import sys
 
-## Put common matheamtica and DRalgo related functions here. Common physics/math functions should go into helpers.py
-
+# Put common matheamtica and DRalgo related functions here.
+# Common physics/math functions should go into helpers.py
 
 def generateMatrixElementsViaSubprocess(filePath: pathlib.Path) -> None:
-    # Command to execute with the given file path
-    command = ["wolframscript", "-file", filePath]
+    # Ensure filePath is string representation of the path
+    filePathStr = str(filePath)
+
+    # Command to execute with the given file path, adjusting for platform
+    if sys.platform == "win32":
+        command = ["wolframscript", "-file", filePathStr]
+    else:  # For Linux and macOS
+        command = ["wolframscript", "-file", filePathStr]
 
     try:
         # run wolframscript
         result = subprocess.run(
-            command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+            command, check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
         )
+        print(result.stdout.decode('utf-8'))  # If you want to print the output
 
     except subprocess.CalledProcessError as e:
         # Handle errors in case the command fails
-        print(
-            "Fatal: Error when generating matrix elements from mathematica via DRalgo"
-        )
+        print("Fatal: Error when generating matrix elements from mathematica via DRalgo")
         print(e.stderr.decode("utf-8"))
