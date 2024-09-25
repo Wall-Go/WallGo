c100f1d77c6975024bb7e4c1aba67c2649ed8837
Jorinde van de Vis <jorindevandevis@outlook.com>
2024-09-25 21:55:53 +0200

    some cleanup in wallgo_example_base

--- Models/wallgo_example_base.py
+++ Models/wallgo_example_base.py
@@ -1,8 +1,11 @@
+"""
+Example base class for computing the wall velocity with WallGo.
+"""
+
 import argparse
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
 import pathlib
-import sys
 import typing
 from pathlib import Path
 import copy
@@ -16,6 +19,8 @@ if typing.TYPE_CHECKING:
 
 @dataclass
 class ExampleInputPoint:
+    """Data class containing model-dependent input"""
+
     inputParameters: dict[str, float]
     phaseInfo: WallGo.PhaseInfo
     veffDerivativeScales: WallGo.VeffDerivativeScales
@@ -26,26 +31,31 @@ class WallGoExampleBase(ABC):
     """Base template for many WallGo examples."""
 
     bShouldRecalculateCollisions: bool
-    """Flag used to check if new collision data should be generated inside the parameter loop.
-    Concrete example models can set this in eg. updateModelParameters() based on their needs.
-    Note that the '--recalculateCollisions' command line flag always takes priority.
+    """Flag used to check if new collision data should be generated inside the
+    parameter loop. Concrete example models can set this in eg. updateModelParameters()
+    based on their needs. Note that the '--recalculateCollisions'
+    command line flag always takes priority.
     """
 
     matrixElementFile: pathlib.Path
-    """Where to load matrix elements from. Used by runExample() if/when the collision model is initialized."""
+    """Where to load matrix elements from. Used by runExample() if/when the
+    collision model is initialized."""
 
     @abstractmethod
     def initWallGoModel(self) -> "WallGo.GenericModel":
-        pass
+        """Initialize the model"""
+        pass  # pylint: disable = W0107
 
     @abstractmethod
     def initCollisionModel(
         self, wallGoModel: WallGo.GenericModel
     ) -> "WallGoCollision.PhysicsModel":
-        pass
+        """Initialize collisions"""
+        pass  # pylint: disable = W0107
 
     @abstractmethod
     def getBenchmarkPoints(self) -> list[ExampleInputPoint]:
+        """Get BM points"""
         return []
 
     @abstractmethod
@@ -53,18 +63,19 @@ class WallGoExampleBase(ABC):
         self, model: "WallGo.GenericModel", inputParameters: dict[str, float]
     ) -> None:
         """Override with whatever non-WallGo logic your example model needs to run
-        when model-specific inputs are changed. Normally this refers to conversion from "physical" input
-        (such as electroweak precision observables) to renormalized Lagrangian parameters,
-        and/or propagating the changes to the effective potential, particle masses, collision model etc.
+        when model-specific inputs are changed. Normally this refers to conversion
+        from "physical" input (such as electroweak precision observables) to
+        renormalized Lagrangian parameters, and/or propagating the changes to
+        the effective potential, particle masses, collision model etc.
         """
-        pass
+        pass  # pylint: disable = W0107
 
     @property
     def exampleBaseDirectory(self) -> Path:
         """Returns directory of the example currently being ran."""
         # Could use sys.modules['__main__'].__file__ to find module containing __main__,
-        # however this only works if the main module is a .py script (won't work eg. in Jupyter).
-        # The following should be safer:
+        # however this only works if the main module is a .py script
+        # (won't work eg. in Jupyter). The following should be safer:
         return pathlib.Path(inspect.getfile(self.__class__)).resolve().parent
 
     def getDefaultCollisionDirectory(self, momentumGridSize: int) -> Path:
@@ -77,22 +88,24 @@ class WallGoExampleBase(ABC):
         inOutCollisionModel: "WallGoCollision.PhysicsModel",
     ) -> None:
         """Override to propagate changes from your WallGo model to the collision model.
-        The base example calls this in runExample() if new collision data needs to be generated
-        inside the parameter loop.
+        The base example calls this in runExample() if new collision data needs to
+        be generated inside the parameter loop.
         """
         raise NotImplementedError(
-            "Must override WallGoBaseExample.updateCollisionModel() to propagate changes from WallGo model to the collision model"
+            """Must override WallGoBaseExample.updateCollisionModel() to propagate
+            changes from WallGo model to the collision model"""
         )
 
     def initCommandLineArgs(self) -> argparse.ArgumentParser:
+        """Define command line arguments for computation settings."""
         argParser = argparse.ArgumentParser()
 
         argParser.add_argument(
             "--momentumGridSize",
             type=int,
             default=0,
-            help="""Basis size N override for momentum grid.
-            Values less than equal to 0 are ignored and we use whatever default the example has defined.""",
+            help="""Basis size N override for momentum grid. Values less than equal
+            to 0 are ignored and we use whatever default the example has defined.""",
         )
 
         argParser.add_argument(
@@ -104,9 +117,11 @@ class WallGoExampleBase(ABC):
         argParser.add_argument(
             "--recalculateCollisions",
             action="store_true",
-            help="""Forces full recalculation of relevant collision integrals instead of loading the provided data files for this example.
-                    This is very slow and disabled by default.
-                    The resulting collision data will be written to a directory labeled _UserGenerated; the default provided data will not be overwritten.
+            help="""Forces full recalculation of relevant collision integrals instead
+                    of loading the provided data files for this example. This is very
+                    slow and disabled by default. The resulting collision data will
+                    be written to a directory labeled _UserGenerated; the default
+                    provided data will not be overwritten.
                     """,
         )
 
@@ -119,17 +134,21 @@ class WallGoExampleBase(ABC):
         return argParser
 
     def assertCollisionModuleAvailable(self) -> None:
-        """Failsafe, in general you should not worry about the collision module being unavailable as long as it has been properly installed (eg. with pip)"""
+        """Failsafe, in general you should not worry about the collision module
+        being unavailable as long as it has been properly installed (eg. with pip)"""
 
-        assert WallGo.isCollisionModuleAvailable(), """WallGoCollision module could not be loaded, cannot proceed with collision integration.
-        Please verify you have successfully installed the module ('pip install WallGoCollision')"""
+        assert WallGo.isCollisionModuleAvailable(), """WallGoCollision module could
+        not be loaded, cannot proceed with collision integration. Please verify you
+        have successfully installed the module ('pip install WallGoCollision')"""
 
     def configureCollisionIntegration(
-        self, inOutCollisionTensor: "WallGoCollision.CollisionTensor"
+        self,
+        inOutCollisionTensor: "WallGoCollision.CollisionTensor",  # pylint: disable=W0613
     ) -> None:
         """Override to do model-specific configuration of collision integration.
-        These settings are a feature of CollisionTensor objects, so the changes must be written directly to the input object.
-        This base class version does nothing, so default options will be used unless overriden.
+        These settings are a feature of CollisionTensor objects, so the changes
+        must be written directly to the input object. This base class version
+        does nothing, so default options will be used unless overriden.
         """
         return
 
@@ -144,11 +163,13 @@ class WallGoExampleBase(ABC):
                 str(self.cmdArgs.momentumGridSize),
             )
 
-    def processResultsForBenchmark(
-        self, benchmarkPoint: ExampleInputPoint, results: WallGo.WallGoResults
+    def processResultsForBenchmark( # pylint: disable=W0613
+        self,
+        benchmarkPoint: ExampleInputPoint,
+        results: WallGo.WallGoResults,
     ) -> None:
-        """Called by runExample() inside its loop over benchmark points after the wall solver finishes.
-        You can override this to eg. write results to a file.
+        """Called by runExample() inside its loop over benchmark points after the wall
+        solver finishes. You can override this to eg. write results to a file.
         The base class version just prints some quantities of interest to stdout."""
 
         header = (
@@ -163,8 +184,11 @@ class WallGoExampleBase(ABC):
         print(f"wallWidths:        {results.wallWidths}")
         print(f"wallOffsets:       {results.wallOffsets}")
 
-    def runExample(self) -> None:
-        """"""
+    def runExample(self) -> None:  # pylint: disable = R0914, R0915
+        """
+        Initializes WallGo and runs the entire model set-up, computation of
+        collision integrals (if enabled) and computation of the wall velocity.
+        """
         WallGo.initialize()
 
         argParser = self.initCommandLineArgs()
@@ -179,11 +203,12 @@ class WallGoExampleBase(ABC):
         model = self.initWallGoModel()
         manager.registerModel(model)
 
-        """Collision model will be initialized only if new collision integrals are needed
-        """
+        """Collision model will be initialized only if new collision integrals
+        are needed"""
         collisionModel: "WallGoCollision.PhysicsModel" | None = None
         """CollisionTensor object to hold and compute the collision integrals,
-        linked to the collision model that creates it. Will be initialized only if new collision integrals are needed.
+        linked to the collision model that creates it. Will be initialized only
+        if new collision integrals are needed.
         """
         collisionTensor: "WallGoCollision.CollisionTensor" | None = None
 
@@ -193,18 +218,22 @@ class WallGoExampleBase(ABC):
         benchmarkPoints = self.getBenchmarkPoints()
         if len(benchmarkPoints) < 1:
             print(
-                "No benchmark points given, did you forget to override WallGoExampleBase.getBenchmarkPoints()?"
+                """\n No benchmark points given, did you forget to
+                override WallGoExampleBase.getBenchmarkPoints()?"""
             )
 
         for benchmark in benchmarkPoints:
 
-            """Update model parameters. Our examples store them internally in the model, through which they propagate to the effective potential.
-            WallGo is not directly aware of model-specific parameters; it only requires EffectivePotential.evaluate() to be valid at field, temperature input,
-            and similarly for particle masses. TODO update collision model
+            """Update model parameters. Our examples store them internally in the model,
+            through which they propagate to the effective potential. WallGo is not
+            directly aware of model-specific parameters; it only requires
+            EffectivePotential.evaluate() to be valid at field, temperature input,
+            and similarly for particle masses.
             """
             self.updateModelParameters(model, benchmark.inputParameters)
 
-            # This needs to run before wallSpeedLTE() or solveWall(), as it does a lot of internal caching related to hydrodynamics
+            # This needs to run before wallSpeedLTE() or solveWall(), as it does a lot
+            # of internal caching related to hydrodynamics
             """WallGo needs info about the phases at
             nucleation temperature. Use the WallGo.PhaseInfo dataclass for this purpose.
             Transition goes from phase1 to phase2.
@@ -223,11 +252,14 @@ class WallGoExampleBase(ABC):
             be close to the LTE result.
             """
 
-            # Take copy of the input solver settings because we will do both off-eq = True/False cases
+            # Take copy of the input solver settings because we will do
+            # both off-eq = True/False cases
             wallSolverSettings = copy.deepcopy(benchmark.wallSolverSettings)
             wallSolverSettings.bIncludeOffEquilibrium = False
 
-            print(f"\n=== Begin EOM with off-eq effects ignored ===")
+            print(
+                f"\n === Begin EOM with off-eq effects ignored ===" # pylint: disable=W1309
+            )
             results = manager.solveWall(wallSolverSettings)
             self.processResultsForBenchmark(benchmark, results)
 
@@ -244,8 +276,10 @@ class WallGoExampleBase(ABC):
                 self.cmdArgs.recalculateCollisions or self.bShouldRecalculateCollisions
             )
 
-            # Specify where to load collision files from. The manager will load them when needed by the internal Boltzmann solver.
-            # Can use existing collision data? => use data packaged with the example. Needs new data? => set new directory and run collision integrator there
+            # Specify where to load collision files from. The manager
+            # will load them when needed by the internal Boltzmann solver.
+            # Can use existing collision data? => use data packaged with the example.
+            # Needs new data? => set new directory and run collision integrator there
             if not bNeedsNewCollisions:
                 manager.setPathToCollisionData(
                     self.getDefaultCollisionDirectory(momentumGridSize)
@@ -258,7 +292,8 @@ class WallGoExampleBase(ABC):
                 )
                 manager.setPathToCollisionData(newCollisionDir)
 
-                ## Initialize collision model if not already done during an earlier benchmark point
+                # Initialize collision model if not already done during
+                # an earlier benchmark point
                 if collisionModel is None or collisionTensor is None:
                     collisionModel = self.initCollisionModel(model)
 
@@ -277,16 +312,20 @@ class WallGoExampleBase(ABC):
                         momentumGridSize
                     )
 
-                    # Setup collision integration settings. Concrete example models can override this to do model-dependent setup
+                    # Setup collision integration settings. Concrete example models
+                    # can override this to do model-dependent setup
                     self.configureCollisionIntegration(collisionTensor)
 
                 else:
                     # collisionModel exists already, update its parameters.
-                    # Note that these changes propagate to all collisionTensor objects that have been created from the collision model
+                    # Note that these changes propagate to all collisionTensor objects
+                    # that have been created from the collision model
                     self.updateCollisionModel(model, collisionModel)
 
-                """Run the collision integrator. This is a very long running function: For M out-of-equilibrium particle species and momentum grid size N,
-                there are order M^2 x (N-1)^4 integrals to be computed. In your own runs you may want to handle this part in a separate script and offload it eg. to a cluster,
+                """Run the collision integrator. This is a very long running function:
+                For M out-of-equilibrium particle species and momentum grid size N,
+                there are order M^2 x (N-1)^4 integrals to be computed. In your own runs
+                you may want to handle this part in a separate script and offload it eg. to a cluster,
                 especially if using N >> 11.
                 """
                 print(
@@ -297,33 +336,38 @@ class WallGoExampleBase(ABC):
                     collisionTensor.computeIntegralsAll()
                 )
 
-                """Export the collision integration results to .hdf5. "individual" means that each off-eq particle pair gets its own file.
-                This format is currently required for the main WallGo routines to understand the data. 
+                """Export the collision integration results to .hdf5. "individual"
+                means that each off-eq particle pair gets its own file. This format is
+                currently required for the main WallGo routines to understand the data. 
                 """
                 collisionResults.writeToIndividualHDF5(
                     str(manager.getCurrentCollisionDirectory())
                 )
 
                 self.bShouldRecalculateCollisions = False
-                ## TODO we could convert the CollisionTensorResult object from above to CollisionArray directly instead of forcing write hdf5 -> read hdf5
+                ## TODO we could convert the CollisionTensorResult object from above to
+                ## CollisionArray directly instead of forcing write hdf5 -> read hdf5
 
             wallSolverSettings.bIncludeOffEquilibrium = True
-            print(f"\n=== Begin EOM with off-eq effects included ===")
+            print(
+                f"\n=== Begin EOM with off-eq effects included ===" # pylint: disable = W1309
+            )
 
             try:
                 results = manager.solveWall(wallSolverSettings)
                 self.processResultsForBenchmark(benchmark, results)
 
-            except Exception as e:
+            except Exception as e:  # pylint: disable = W0718
                 print(f"Unhandled exception from wall solver: {e}")
                 if isinstance(e, WallGo.CollisionLoadError):
                     print(
                         """\nLoad of collision integrals failed!
                         WallGo example models come with pre-generated collision files,
-                        so load failure here probably means you've either moved files around
-                        or changed to incompatible grid size.
-                        If you were trying to generate your own collision data, make sure to
-                        include the --recalculateCollisions command line flag when running the example.
+                        so load failure here probably means you've either moved files
+                        around or changed to incompatible grid size. If you were trying
+                        to generate your own collision data, make sure to include the 
+                        --recalculateCollisions command line flag when running
+                        the example.
                         """
                     )
 
