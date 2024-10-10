a263446326791c449e6691611c9bd6cc56da5eaf
Jorinde van de Vis <jorindevandevis@outlook.com>
2024-10-10 16:35:36 +0200

    now it runs

--- Models/SimpleModel/simpleModelBasic.py
+++ Models/SimpleModel/simpleModelBasic.py
@@ -4,27 +4,13 @@ fermion. The model is based on the Yukawa model, but the masses
 and interactions are treated as independent (this is not
 physical, but makes the computation simpler).
 """
-
-import sys
 import pathlib
-from typing import TYPE_CHECKING
 from pathlib import Path
 import numpy as np
 
 # WallGo imports
 import WallGo  # Whole package, in particular we get WallGo._initializeInternal()
 from WallGo import Fields, GenericModel, Particle
-
-# Add the Models folder to the path; need to import the base
-# example template
-modelsBaseDir = pathlib.Path(__file__).resolve().parent.parent
-sys.path.append(str(modelsBaseDir))
-
-from wallGoExampleBase import WallGoExampleBase  # pylint: disable=C0411, C0413, E0401
-from wallGoExampleBase import ExampleInputPoint  # pylint: disable=C0411, C0413, E0401
-
-if TYPE_CHECKING:
-    import WallGoCollision
 
 
 class SimpleModel(GenericModel):
@@ -53,32 +39,23 @@ class SimpleModel(GenericModel):
     def getEffectivePotential(self) -> "EffectivePotentialSimple":
         return self.effectivePotential
 
-    # ~
-
     def defineParticles(self) -> None:
         """
-        Define the particles for the model.
-        Note that the particle list only needs to contain the
-        particles that are relevant for the Boltzmann equations.
-        The particles relevant to the effective potential are
-        included independently.
+        Define the out-of-equilibrium particles for the model.
         """
         self.clearParticles()
 
-        # === left fermion ===
-        # The msqVacuum function of an out-of-equilibrium particle must take
-        # a Fields object and return an array of length equal to the number of
-        # points in fields.
+        # Vacuum mass squared
         def psiMsqVacuum(fields: Fields) -> Fields:
             return self.modelParameters["mf"] + self.modelParameters[
                 "yf"
             ] * fields.getField(0)
-
-        # The msqDerivative function of an out-of-equilibrium particle must take
-        # a Fields object and return an array with the same shape as fields.
+        
+        # Field-derivative of the vacuum mass squared
         def psiMsqDerivative(fields: Fields) -> Fields:  # pylint: disable = W0613
             return self.modelParameters["yf"]
 
+        # Thermal lass
         def psiMsqThermal(T: float) -> float:
             return 1 / 16 * self.modelParameters["yf"] ** 2 * T**2
 
@@ -107,10 +84,9 @@ class SimpleModel(GenericModel):
         self, inputParameters: dict[str, float]
     ) -> dict[str, float]:
         """
-        Calculate Lagrangian parameters based on the input parameters.
-        Here the model parameters are direct input.
+        Calculate parameters for potential and collisions, based on the input
+        parameters. Here the model parameters are direct input.
         """
-        # Parameters for the potential and the collisions
         self.modelParameters = inputParameters
 
         return self.modelParameters
@@ -190,36 +166,6 @@ class EffectivePotentialSimple(WallGo.EffectivePotential):
 
         return np.array(potentialTotal)
 
-
-class SimpleModelExample():
-    """
-    Sets up the model, computes or loads the collison
-    integrals, and computes the wall velocity.
-    """
-
-    def __init__(self) -> None:
-        """"""
-        self.bShouldRecalculateCollisions = False
-
-
-    def initWallGoModel(self) -> "WallGo.GenericModel":
-        """
-        Initialize the model.
-        """
-        return SimpleModel()
-
-    def getDefaultCollisionDirectory(self, momentumGridSize: int) -> Path:
-        """Path to the directory containing default collision data for the example."""
-        return self.exampleBaseDirectory / Path(f"CollisionOutput_N{momentumGridSize}")
-
-    def configureManager(self, inOutManager: "WallGo.WallGoManager") -> None:
-        """We use a spatial grid size = 20"""
-        super().configureManager(inOutManager)
-        inOutManager.config.set("PolynomialGrid", "spatialGridSize", "20")
-
-    # ~ End WallGoExampleBase interface
-
-
 def main() -> None:
 
     manager = WallGo.WallGoManager()
@@ -251,8 +197,6 @@ def main() -> None:
                         50.0,
                     ],
                 ),
-
-    print(veffDerivativeScales)
     
     manager.setupThermodynamicsHydrodynamics(
                 phaseInfo,
@@ -263,7 +207,33 @@ def main() -> None:
                     ],
                 )
             )
+    
+    momentumGridSize = 11
+    pathtoCollisions = pathlib.Path(__file__).resolve().parent/ Path(f"CollisionOutput_N{momentumGridSize}")
+    
+    manager.setPathToCollisionData(pathtoCollisions)
+    
+    # ---- Solve wall speed in Local Thermal Equilibrium (LTE) approximation
+    vwLTE = manager.wallSpeedLTE()
+    print(f"LTE wall speed:    {vwLTE:.6f}")
 
+    results = manager.solveWall(WallGo.WallSolverSettings(
+                    bIncludeOffEquilibrium=False,
+                    meanFreePathScale=1000.0, # In units of 1/Tnucl
+                    wallThicknessGuess=5.0, # In units of 1/Tnucl
+                )
+    )
+
+    print(f"Wall velocity without out-of-equilibrium contributions {results.wallVelocity}")
+
+    results = manager.solveWall(WallGo.WallSolverSettings(
+                    bIncludeOffEquilibrium=True,
+                    meanFreePathScale=1000.0, # In units of 1/Tnucl
+                    wallThicknessGuess=5.0, # In units of 1/Tnucl
+                )
+    )
+
+    print(f"Wall velocity with out-of-equilibrium contributions {results.wallVelocity}")
 
 
 
