638a4751ec011c43063fffe0e972e53971915a40
Benoit Laurent <benoit.laurent96@gmail.com>
2024-10-15 20:01:55 -0400

    Replaced all the calls to print() in the source code by calls to logging with an appropriate level

--- src/WallGo/wallGoManager.py
+++ src/WallGo/wallGoManager.py
@@ -5,6 +5,7 @@ wall velocity calculation.
 
 from dataclasses import dataclass
 import pathlib
+import logging
 import numpy as np
 
 # WallGo imports
@@ -77,6 +78,8 @@ class WallGoManager:
 
         # Initialise the configs with the default values
         self.config = Config()
+        
+        logging.basicConfig(format='%(message)s', level=logging.INFO, force=True)
 
         # default to working directory
         self.collisionDirectory = pathlib.Path.cwd()
@@ -140,13 +143,13 @@ class WallGoManager:
         self.initTemperatureRange()
 
         ## Should we write these to a result struct?
-        print("Temperature ranges:")
-        print(
+        logging.info("Temperature ranges:")
+        logging.info(
             "High-T phase: TMin = "
             f"{self.thermodynamics.freeEnergyHigh.minPossibleTemperature[0]}, "
             f"TMax = {self.thermodynamics.freeEnergyHigh.maxPossibleTemperature[0]}"
         )
-        print(
+        logging.info(
             "Low-T phase: TMin = "
             f"{self.thermodynamics.freeEnergyLow.minPossibleTemperature[0]}, "
             f"TMax = {self.thermodynamics.freeEnergyLow.maxPossibleTemperature[0]}"
@@ -168,7 +171,7 @@ class WallGoManager:
                 },
             )
 
-        print(f"Jouguet: {self.hydrodynamics.vJ}")
+        logging.info(f"Jouguet: {self.hydrodynamics.vJ}")
         # TODO return some results struct
 
     def isModelValid(self) -> bool:
@@ -221,8 +224,8 @@ class WallGoManager:
             phaseInput.phaseLocation2, T
         )
 
-        print(f"Found phase 1: phi = {phaseLocation1}, Veff(phi) = {effPotValue1}")
-        print(f"Found phase 2: phi = {phaseLocation2}, Veff(phi) = {effPotValue2}")
+        logging.info(f"Found phase 1: phi = {phaseLocation1}, Veff(phi) = {effPotValue1}")
+        logging.info(f"Found phase 2: phi = {phaseLocation2}, Veff(phi) = {effPotValue2}")
 
         if np.allclose(phaseLocation1, phaseLocation2, rtol=1e-05, atol=1e-05):
             raise WallGoPhaseValidationError(
@@ -287,7 +290,7 @@ class WallGoManager:
             # required temperature. We do not solve hydrodynamics inside the bubble, so
             # we are only interested in T- (the temperature right at the wall).
             hydrodynamicsTemplate = HydrodynamicsTemplateModel(self.thermodynamics)
-            print(f"vwLTE in the template model: {hydrodynamicsTemplate.findvwLTE()}")
+            logging.info(f"vwLTE in the template model: {hydrodynamicsTemplate.findvwLTE()}")
 
         except WallGoError as error:
             # Throw new error with more info
