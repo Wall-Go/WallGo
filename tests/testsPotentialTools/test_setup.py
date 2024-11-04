e4f3b569d50599b7b5419ddbfa457a6aeb3ff791
Oliver Gould <oliver.gould@nottingham.ac.uk>
2024-11-04 14:45:44 +0000

    Updated tests to include both 3.10 and 3.13

--- tests/testsPotentialTools/test_setup.py
+++ tests/testsPotentialTools/test_setup.py
@@ -4,8 +4,3 @@
 def test_importPotentialTools() -> None:
     """Testing import of PotentialTools from WallGo"""
     from WallGo import PotentialTools
-
-
-def test_importPotentialToolsAlternative() -> None:
-    """Testing import of WallGo.PotentialTools"""
-    import WallGo.PotentialTools
